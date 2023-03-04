885f9121c553e81f8e0c1644436350ce29c44c67
albertonav <alberto.navaro@gmail.com>
2023-03-04 23:27:38 +0100

    add proxi to esbuild to refresh custom assets

--- development.config.js
+++ development.config.js
@@ -1,5 +1,9 @@
 import * as esbuild from 'esbuild';
 import { sassPlugin } from 'esbuild-sass-plugin';
+import http from 'node:http';
+import chokidar from 'chokidar';
+
+const clients = [];
 
 await esbuild.build({
     entryPoints: ['src/scss/style.scss', 'src/js/main.js'],
@@ -8,7 +12,7 @@ await esbuild.build({
     plugins: [sassPlugin()],
 });
 
-let ctx = await esbuild.context({
+const ctx = await esbuild.context({
     entryPoints: ['src/scss/style.scss', 'src/js/main.js'],
     bundle: true,
     sourcemap: true,
@@ -18,8 +22,43 @@ let ctx = await esbuild.context({
 
 await ctx.watch();
 
-let { host, port } = await ctx.serve({
+const { host, port } = await ctx.serve({
     servedir: 'dist',
 });
 
-console.log(host, port);
+// Then start a proxy server on port 3000
+http.createServer((req, res) => {
+    const options = {
+        hostname: host,
+        port: port,
+        path: req.url,
+        method: req.method,
+        headers: req.headers,
+    };
+
+    clients.push(res);
+
+    // Forward each incoming request to esbuild
+    const proxyReq = http.request(options, (proxyRes) => {
+        // If esbuild returns "not found", send a custom 404 page
+        // if (proxyRes.statusCode === 404) {
+        //     res.writeHead(404, { 'Content-Type': 'text/html' });
+        //     res.end('<h1>A custom 404 page</h1>');
+        //     return;
+        // }
+
+        // Otherwise, forward the response from esbuild to the client
+        res.writeHead(proxyRes.statusCode, proxyRes.headers);
+        proxyRes.pipe(res, { end: true });
+    });
+
+    // Forward the body of the request to esbuild
+    req.pipe(proxyReq, { end: true });
+}).listen(3000);
+
+chokidar.watch('dist/**/*.html').on('change', () => {
+    clients.forEach((res) => res.write('data: update\n\n'));
+    clients.length = 0;
+});
+
+console.log(host, '3000');
--- dist/scripts/reload.js
+++ dist/scripts/reload.js
@@ -1,1 +1,7 @@
 new EventSource('/esbuild').addEventListener('change', () => location.reload());
+new EventSource('/esbuild').addEventListener('message', (event) => {
+    const { data } = event;
+    if (data !== 'update') return;
+
+    location.reload();
+});
