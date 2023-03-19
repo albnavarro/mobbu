917b754e3d89d70b6929f0e3a5cfc11c3e38c5f8
albertonav <alberto.navaro@gmail.com>
2023-03-19 02:08:32 +0100

    Fix navigation button.

--- src/js/component/navigation/navigationButton.js
+++ src/js/component/navigation/navigationButton.js
@@ -5,7 +5,7 @@ export const NavigationButton = ({ props, render, onMount }) => {
         });
     });
 
-    const { label, url, arrowClass, subMenuClass } = props;
+    const { label = '', url = '#', arrowClass = '', subMenuClass = '' } = props;
     return render(`
         <button class="l-navigation__link ${arrowClass} ${subMenuClass}">
             ${label}
