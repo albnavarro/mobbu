d42096ed4a7c06bb81bb562278454438ab15b57f
alberto <alberto.navaro@gmail.com>
2023-04-21 18:28:00 +0200

    fix(home) style

--- src/js/component/pages/canvas/caterpillarCanvas/animation/animation.js
+++ src/js/component/pages/canvas/caterpillarCanvas/animation/animation.js
@@ -1,5 +1,19 @@
+import { core } from '../../../../../mobbu';
+
 export const caterpillarCanvasAnimation = ({ canvas }) => {
     console.log(canvas);
 
-    return () => {};
+    const unsubscribeResize = core.useResize(() => {
+        canvas.width = canvas.clientWidth;
+        canvas.height = canvas.clientHeight;
+        draw();
+    });
+
+    const draw = () => {
+        console.log('draw');
+    };
+
+    return () => {
+        unsubscribeResize();
+    };
 };
