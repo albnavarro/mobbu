63a99b3a6516eb1bf657fc1d00234ec879c33a49
alberto <alberto.navaro@gmail.com>
2023-04-23 00:39:48 +0200

    chore(header) add routing to top link.

--- src/js/component/layout/header/headernav.js
+++ src/js/component/layout/header/headernav.js
@@ -1,4 +1,5 @@
 import { getCommonData } from '../../../route';
+import { navigationStore } from '../navigation/store/navStore';
 
 function additems() {
     const { header } = getCommonData();
@@ -6,14 +7,25 @@ function additems() {
 
     return links
         .map((link) => {
-            const { label, url } = link;
-            return `
-                <li class="l-header__sidenav__item">
-                    <a href="${url}" class="l-header__sidenav__link">
-                        ${label}
-                    </a>
-                </li>
-            `;
+            const { label, url, internal } = link;
+
+            return /* HTML */ ` <li class="l-header__sidenav__item">
+                ${!internal
+                    ? /* HTML */ `
+                          <a href="${url}" class="l-header__sidenav__link">
+                              ${label}
+                          </a>
+                      `
+                    : /* HTML */ `
+                          <button
+                              type="button"
+                              data-url="${url}"
+                              class="l-header__sidenav__link"
+                          >
+                              ${label}
+                          </button>
+                      `}
+            </li>`;
         })
         .join('');
 }
@@ -21,7 +33,20 @@ function additems() {
 /**
  * Create component
  */
-export const Headernav = ({ render }) => {
+export const Headernav = ({ render, onMount }) => {
+    onMount(({ element }) => {
+        const buttons = element.querySelectorAll('button');
+
+        [...buttons].forEach((button) => {
+            button.addEventListener('click', () => {
+                const { url } = button.dataset;
+                window.location.hash = url;
+                navigationStore.set('navigationIsOpen', false);
+                navigationStore.emit('closeNavigation');
+            });
+        });
+    });
+
     return render(/* HTML */ `
         <ul class="l-header__sidenav">
             ${additems()}
