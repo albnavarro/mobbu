f5645a8c2d789d7d3285f7c57579ea9144deec85
alberto <alberto.navaro@gmail.com>
2023-04-23 18:27:43 +0200

    chore(home) content basic animation.

--- src/js/component/pages/homepage/homeContent.js
+++ src/js/component/pages/homepage/homeContent.js
@@ -1,11 +1,34 @@
+import { tween } from '../../../mobbu';
+
 export const HomeContent = ({ onMount, render }) => {
     onMount(({ element }) => {
-        console.log(element);
+        const title = element.querySelector('.js-title');
+
+        let contentTween = tween.createTween({
+            data: { opacity: 0, scale: 1.1 },
+            stagger: { each: 4 },
+            duration: 500,
+            ease: 'easeOutQuad',
+        });
+
+        [title].forEach((item) => {
+            contentTween.subscribe(({ opacity, scale }) => {
+                item.style.opacity = opacity;
+                item.style.transform = `scale(${scale}) translateZ(1px)`;
+            });
+        });
+
+        contentTween.goTo({ opacity: 1, scale: 1 });
+
+        return () => {
+            contentTween.destroy();
+            contentTween = null;
+        };
     });
 
     return render(/* HTML */ `
         <div class="l-index__content">
-            <h1 class="l-index__content__title">Lorem ipsum</h1>
+            <h1 class="l-index__title js-title">Lorem ipsum</h1>
         </div>
     `);
 };
