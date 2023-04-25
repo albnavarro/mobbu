49c96d34fbee0d6268bc553d7ab5f71f0460a46a
alberto <alberto.navaro@gmail.com>
2023-04-25 15:51:50 +0200

    feat(animatedpattern) add besic timeline

--- src/js/component/pages/animatedPattern/animatedPatternN0/animation/animation.js
+++ src/js/component/pages/animatedPattern/animatedPatternN0/animation/animation.js
@@ -1,12 +1,13 @@
-import { core } from '../../../../../mobbu';
+import { core, timeline, tween } from '../../../../../mobbu';
 import { createGrid, roundRectCustom } from '../../../../../utils/canvasUtils';
 import { navigationStore } from '../../../../layout/navigation/store/navStore';
-import { originalData } from '../../../test/data';
 
 export const animatedPatternN0Animation = ({ canvas }) => {
     let isActive = true;
     let gridData = [];
     let data = [];
+    let gridTween = {};
+    let gridTimeline = {};
     let ctx = canvas.getContext('2d', { alpha: false });
 
     const nRow = 7;
@@ -21,6 +22,26 @@ export const animatedPatternN0Animation = ({ canvas }) => {
     gridData = createGrid({ nRow, nCol, cellWidth, cellHeight, gutter }).items;
     data = gridData.map((item) => {
         return { ...item, ...{ opacity: 0, rotate: 0 } };
+    });
+
+    gridTween = tween.createTween({
+        ease: 'easeInOutQuad',
+        stagger: {
+            each: 15,
+            from: 'center',
+            // from: { x: 23, y: 10 },
+            grid: { col: 45, row: 45, direction: 'row' },
+            waitComplete: false,
+        },
+        data: { scale: 1, rotate: 0, opacity: 1 },
+    });
+
+    data.forEach((item) => {
+        gridTween.subscribeCache(item, ({ scale, rotate, opacity }) => {
+            item.rotate = rotate;
+            item.opacity = opacity;
+            item.scale = scale;
+        });
     });
 
     const draw = () => {
@@ -38,19 +59,21 @@ export const animatedPatternN0Animation = ({ canvas }) => {
         ctx.fillStyle = '#f6f6f6';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        data.forEach(({ x, y, width, height }) => {
+        data.forEach(({ x, y, width, height, rotate, scale }) => {
             ctx.save();
-
-            ctx.translate(centerX + width / 2, centerY + height / 2);
-            ctx.translate(
-                parseInt(-centerX - width / 2),
-                parseInt(-centerY - height / 2)
-            );
 
             const halfColLenght =
                 ((width + gutter) * (nCol + 1)) / 2 - gutter / 2;
             const halfRowLenght =
                 ((height + gutter) * (nRow + 2)) / 2 - gutter / 2;
+
+            ctx.translate(centerX, centerY);
+            ctx.rotate((Math.PI / 180) * rotate);
+            ctx.scale(scale, scale);
+            ctx.translate(
+                parseInt(-centerX - width / 2),
+                parseInt(-centerY - height / 2)
+            );
 
             roundRectCustom(
                 ctx,
@@ -69,6 +92,17 @@ export const animatedPatternN0Animation = ({ canvas }) => {
             ctx.restore();
         });
     };
+
+    gridTimeline = timeline
+        .createAsyncTimeline({ repeat: -1, yoyo: true })
+        .goTo(gridTween, { scale: 1.5 }, { duration: 1000 })
+        .goTo(gridTween, { scale: 0.5 }, { duration: 500 })
+        .goTo(gridTween, { rotate: 180, scale: 1.2 }, { duration: 500 })
+        .goTo(gridTween, { scale: 1.3 }, { duration: 500 })
+        .goTo(gridTween, { opacity: 0.5 }, { duration: 1200 })
+        .goTo(gridTween, { opacity: 1, scale: 1 }, { duration: 1200 });
+
+    gridTimeline.play();
 
     /**
      * Loop
@@ -117,9 +151,13 @@ export const animatedPatternN0Animation = ({ canvas }) => {
     canvas.classList.add('active');
 
     return () => {
+        gridTween.destroy();
+        gridTimeline.destroy();
         unsubscribeResize();
         unWatchResume();
         unWatchPause();
+        gridTween = null;
+        gridTimeline = null;
         ctx = null;
         gridData = [];
         data = [];
