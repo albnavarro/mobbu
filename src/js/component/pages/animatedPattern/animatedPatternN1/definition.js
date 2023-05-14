7b05728e61516f69119fbc06b1af09fd4e0325c3
alberto <alberto.navaro@gmail.com>
2023-05-14 22:12:35 +0200

    chore(animation) refine

--- src/js/component/pages/animatedPattern/animatedPatternN1/definition.js
+++ src/js/component/pages/animatedPattern/animatedPatternN1/definition.js
@@ -13,7 +13,7 @@ export const animatedPatternN1Def = createComponentDefinition({
         gutter: 10,
         fill: [
             21, 22, 23, 24, 25, 25, 26, 37, 42, 53, 58, 69, 74, 85, 86, 87, 88,
-            89, 90,
+            89, 90, 44, 60, 65, 66,
         ],
         disableOffcanvas: detectFirefox() || detectSafari() ? true : false,
     },
