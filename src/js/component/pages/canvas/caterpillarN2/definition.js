41dc48a0ea7eea84f691f8a837a1b46d35048ffa
alberto <alberto.navaro@gmail.com>
2023-05-14 15:12:51 +0200

    chore(format code)

--- src/js/component/pages/canvas/caterpillarN2/definition.js
+++ src/js/component/pages/canvas/caterpillarN2/definition.js
@@ -5,25 +5,46 @@ import { CaterpillarN2 } from './caterpillarN2';
 const duration = 10;
 
 const buttons = {
-    'js-CN2-play': { label: 'play', method: 'play' },
-    'js-CN2-playReverse': { label: 'play reverse', method: 'playReverse' },
+    'js-CN2-play': {
+        label: 'play',
+        method: 'play',
+    },
+    'js-CN2-playReverse': {
+        label: 'play reverse',
+        method: 'playReverse',
+    },
     'js-CN2-play-current': {
-        label: 'play from current',
+        label: 'go forward if is playing backward',
         method: 'playUseCurrent',
     },
     'js-CN2-playReverse-current': {
-        label: 'play reverse from current',
+        label: 'go backward if is playing forward',
         method: 'playReverseUseCurrent',
     },
-    'js-CN2-play-label': { label: 'play from label', method: 'playFromLabel' },
+    'js-CN2-play-label': {
+        label: 'play from label',
+        method: 'playFromLabel',
+    },
     'js-CN2-playReverse-label': {
         label: 'play from label reverse',
         method: 'plaFromLabelReverse',
     },
-    'js-CN2-reverse': { label: 'reverse', method: 'reverse' },
-    'js-CN2-stop': { label: 'stop', method: 'stop' },
-    'js-CN2-pause': { label: 'pause', method: 'pause' },
-    'js-CN2-resume': { label: 'resume', method: 'resume' },
+    'js-CN2-reverse': {
+        label: 'reverse',
+        method: 'reverse',
+    },
+    'js-CN2-stop': {
+        label: 'stop',
+        method: 'stop',
+    },
+    'js-CN2-pause': {
+        label: 'pause',
+        method: 'pause',
+    },
+    'js-CN2-resume': {
+        label: 'resume',
+        method: 'resume',
+    },
 };
 
 export const caterpillarN2Def = createComponentDefinition({
