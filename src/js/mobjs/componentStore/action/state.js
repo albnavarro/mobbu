b9619b54ac03c57b96e0165190f059bad7213148
alberto <alberto.navaro@gmail.com>
2023-06-18 23:23:15 +0200

    chore(ts-check)

--- src/js/mobjs/componentStore/action/state.js
+++ src/js/mobjs/componentStore/action/state.js
@@ -1,11 +1,18 @@
 import { componentStore } from '../store';
 
 /**
+ * @param {string} id
+ * @return Object
+ *
+ * @description
  * Get state
  */
-export const getStateById = (id) => {
-    if (!id) return null;
+export const getStateById = (id = '') => {
+    if (!id || id === '') return;
 
+    /**
+     * @type {{instances: Array.<import('../store.js').componentStoreType >}}
+     */
     const { instances } = componentStore.get();
     const instance = instances.find(({ id: currentId }) => currentId === id);
 
@@ -19,11 +26,21 @@ export const getStateById = (id) => {
 };
 
 /**
+ * @param {String} id
+ * @param {String} prop
+ * @param {Any} value
+ * @param {Boolean} fire
+ * @returns void
+ *
+ * @description
  * Set state
  */
-export const setStateById = (id, prop, value, fire = true) => {
-    if (!id && !prop && !value) return;
+export const setStateById = (id = '', prop = '', value, fire = true) => {
+    if ((!id || id === '') && (!prop || prop === '') && !value) return;
 
+    /**
+     * @type {{instances: Array.<import('../store.js').componentStoreType >}}
+     */
     const { instances } = componentStore.get();
     const instance = instances.find(({ id: currentId }) => currentId === id);
 
--- src/js/mobjs/componentStore/action/watch.js
+++ src/js/mobjs/componentStore/action/watch.js
@@ -1,18 +1,29 @@
+// @ts-check
+
 import { componentStore } from '../store';
 
 /**
+ * @param {string} id
+ * @param {string} prop
+ * @param {function} cb
+ * @returns {(Promise.<Function>|undefined)}
+ *
+ * @description
  * Watch state
  */
-export const watchById = (id, prop, cb) => {
-    if (!id && !prop && !cb) return;
+export const watchById = (id = '', prop = '', cb = () => {}) => {
+    if ((!id || id === '') && (!prop || prop === '')) return undefined;
 
+    /**
+     * @type {{instances: Array.<import('../store.js').componentStoreType >}}
+     */
     const { instances } = componentStore.get();
     const instance = instances.find(({ id: currentId }) => currentId === id);
 
     const state = instance?.state;
     if (!state) {
         console.warn(`watchById failed no id found on prop: ${prop}`);
-        return null;
+        return undefined;
     }
 
     return state.watch(prop, cb);
