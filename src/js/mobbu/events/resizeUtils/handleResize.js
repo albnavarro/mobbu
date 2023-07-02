8cff329bb94fbc92cc0d7d7cd5048f376d577a59
alberto <alberto.navaro@gmail.com>
2023-07-02 20:51:13 +0200

    feat: useScroll: remove closure.

--- src/js/mobbu/events/resizeUtils/handleResize.js
+++ src/js/mobbu/events/resizeUtils/handleResize.js
@@ -1,4 +1,151 @@
+// @ts-check
+
 import { debounceFuncion } from '../debounce.js';
+
+/**
+ * @type {Boolean}
+ */
+let inizialized = false;
+
+/**
+ * @type {Array.<{id:number, cb:Function }>}
+ */
+let callback = [];
+
+/**
+ * @type {Number}
+ */
+let id = 0;
+
+/**
+ * @type {Function}
+ */
+let debouceFunctionReference = () => {};
+
+/**
+ * @type {number}
+ */
+let previousWindowHeight = window.innerHeight;
+
+/**
+ * @type {number}
+ */
+let previousWindowWidth = window.innerWidth;
+
+/**
+ * @returns void
+ */
+function handler() {
+    /**
+     * If there is no subscritor remove handler.
+     */
+    if (callback.length === 0) {
+        // @ts-ignore
+        window.removeEventListener('resize', debouceFunctionReference);
+
+        inizialized = false;
+        return;
+    }
+
+    // Check there is a vertical resizer
+
+    /**
+     * @type {number}
+     */
+    const windowsHeight = window.innerHeight;
+
+    /**
+     * @type {number}
+     */
+    const windowsWidth = window.innerWidth;
+
+    /**
+     * @type {Boolean}
+     */
+    const verticalResize = windowsHeight !== previousWindowHeight;
+
+    /**
+     * @type {Boolean}
+     */
+    const horizontalResize = windowsWidth !== previousWindowWidth;
+
+    /**
+     * @type {Number}
+     */
+    previousWindowHeight = windowsHeight;
+
+    /**
+     * @type {Number}
+     */
+    previousWindowWidth = windowsWidth;
+
+    // Prepare data to callback
+    const resizeData = {
+        scrollY: window.pageYOffset,
+        windowsHeight,
+        windowsWidth,
+        documentHeight: document.documentElement.scrollHeight,
+        verticalResize,
+        horizontalResize,
+    };
+
+    // Fire end of resize
+    callback.forEach(({ cb }) => cb(resizeData));
+}
+
+/**
+ * init - if listener is not inizializad add it
+ *
+ * @return {void}
+ */
+function init() {
+    if (inizialized) return;
+    inizialized = true;
+
+    // Add debunce function to detect scroll end
+    debouceFunctionReference = debounceFuncion(() => handler());
+    // @ts-ignore
+    window.addEventListener('resize', debouceFunctionReference, {
+        passive: false,
+    });
+}
+
+/**
+ * @description
+ * Add callback on resize using a debounce function.
+ *
+ * @param {function(handleResizeTypes):void } cb - callback function fired on resize.
+ *
+ * @example
+ * ```js
+ * handleResize(
+ *     ({
+ *         documentHeight,
+ *         horizontalResize,
+ *         scrollY,
+ *         verticalResize,
+ *         windowsHeight,
+ *         windowsWidth,
+ *     }) => {
+ *         // code
+ *     }
+ * );
+ *
+ * ```
+ */
+const addCb = (cb) => {
+    callback.push({ cb, id: id });
+    const cbId = id;
+    id++;
+
+    if (typeof window !== 'undefined') {
+        init();
+    }
+
+    return () => {
+        callback = callback.filter((item) => item.id !== cbId);
+    };
+};
 
 /**
  * @typedef {Object} handleResizeTypes
@@ -9,105 +156,4 @@ import { debounceFuncion } from '../debounce.js';
  * @prop {boolean} verticalResize - Boolean value indicating whether the height of the viewport changed during the resize.
  * @prop {boolean} horizontalResize - Boolean value indicating whether the width of the viewport changed during the resize.
  */
-
-export const handleResize = (() => {
-    let inizialized = false;
-    let callback = [];
-    let id = 0;
-    let debouceFunctionReference = () => {};
-    let previousWindowHeight = window.innerHeight;
-    let previousWindowWidth = window.innerWidth;
-
-    /**
-     *
-     */
-    function handler() {
-        /**
-         * if - if there is no subscritor remove handler
-         */
-        if (callback.length === 0) {
-            window.removeEventListener('resize', debouceFunctionReference);
-
-            inizialized = false;
-            return;
-        }
-
-        // Check there is a vertical resizer
-        const windowsHeight = window.innerHeight;
-        const windowsWidth = window.innerWidth;
-        const verticalResize = windowsHeight !== previousWindowHeight;
-        const horizontalResize = windowsWidth !== previousWindowWidth;
-        previousWindowHeight = windowsHeight;
-        previousWindowWidth = windowsWidth;
-
-        // Prepare data to callback
-        const resizeData = {
-            scrollY: window.pageYOffset,
-            windowsHeight,
-            windowsWidth,
-            documentHeight: document.documentElement.scrollHeight,
-            verticalResize,
-            horizontalResize,
-        };
-
-        // Fire end of resize
-        callback.forEach(({ cb }) => {
-            cb(resizeData);
-        });
-    }
-
-    /**
-     * init - if listener is not inizializad add it
-     *
-     * @return {void}
-     */
-    function init() {
-        if (inizialized) return;
-        inizialized = true;
-
-        // Add debunce function to detect scroll end
-        debouceFunctionReference = debounceFuncion((e) => handler(e));
-        window.addEventListener('resize', debouceFunctionReference, {
-            passive: false,
-        });
-    }
-
-    /**
-     * @description
-     * Add callback on resize using a debounce function.
-     *
-     * @param {function(handleResizeTypes):void } cb - callback function fired on resize.
-     *
-     * @example
-     * ```js
-     * handleResize(
-     *     ({
-     *         documentHeight,
-     *         horizontalResize,
-     *         scrollY,
-     *         verticalResize,
-     *         windowsHeight,
-     *         windowsWidth,
-     *     }) => {
-     *         // code
-     *     }
-     * );
-     *
-     * ```
-     */
-    const addCb = (cb) => {
-        callback.push({ cb, id: id });
-        const cbId = id;
-        id++;
-
-        if (typeof window !== 'undefined') {
-            init();
-        }
-
-        return () => {
-            callback = callback.filter((item) => item.id !== cbId);
-        };
-    };
-
-    return addCb;
-})();
+export const handleResize = (() => addCb)();
