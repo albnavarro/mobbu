751aee776d82e2e0b029d39359995e67b6e97a64
alberto <alberto.navaro@gmail.com>
2023-07-22 19:56:25 +0200

    Feat: set callback add ts-check.

--- src/js/mobbu/animation/utils/callbacks/setCallback.js
+++ src/js/mobbu/animation/utils/callbacks/setCallback.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 import { callBackStore } from './callBackStore';
 import { handleCache } from '../../../events/rafutils/handleCache.js';
 
@@ -6,36 +8,79 @@ import { handleCache } from '../../../events/rafutils/handleCache.js';
  * @param {Object.<string, number>} props
  */
 
-export const setCallBack = (cb, arr) => {
+/**
+ * @param {Function} currentCallback - callback to execute.
+ * @param {Array.<{cb:function,id:number,index:Number,frame:Number}>} arrayOfCallback
+ * @returns {function(Array.<{cb:function,id:number,index:Number,frame:Number}>):Array.<{cb:function,id:number,index:Number,frame:Number}>}
+ *
+ * @description
+ * Add callback to Stack.
+ *
+ */
+export const setCallBack = (currentCallback, arrayOfCallback) => {
+    // Get new unbubscribeId.
     const { id } = callBackStore.get();
-    arr.push({ cb, id });
+
+    // Update main callback array.
+    // index and frame is settled on first run of tween
+    arrayOfCallback.push({ cb: currentCallback, id, index: -1, frame: -1 });
+
+    // Save current id for unsubscribe.
     const prevId = id;
+
+    // Update Incremental id.
     callBackStore.set('id', id + 1);
 
-    // Disable single stagger without modify staggers order
-    return (arr) =>
-        arr.map(({ id, cb, index, frame }) => {
+    // Delete item from arrayOfCallback, it is possibile unsubscribe a single stagger.
+    return (arrayOfCallback) =>
+        arrayOfCallback.map(({ id, cb, index, frame }) => {
+            // Set NOOP.
             if (id === prevId) cb = () => {};
             return { id, cb, index, frame };
         });
 };
 
-export const setCallBackCache = (item, fn, cbArray, unsubscribeCache) => {
+/**
+ * @param {('Object'|'HTMLElement')} item
+ * @param {function(Object.<string, number>):void} currentCallback
+ * @param {Array.<{cb:number,id:number,index:Number,frame:Number}>} arrayOfCallback
+ * @param {Array.<function>} unsubscribeCacheArray - unsubscribe function of handleCache.
+ *
+ * @returns {{ unsubscribeCache:Array.<function>, unsubscribeCb:function(Array.<{cb:number,id:number,index:Number,frame:Number}>):Array.<{cb:number,id:number,index:Number,frame:Number}> }}
+ */
+export const setCallBackCache = (
+    item,
+    currentCallback,
+    arrayOfCallback,
+    unsubscribeCacheArray
+) => {
+    // Get new unbubscribeId.
     const { id } = callBackStore.get();
-    const { id: cacheId, unsubscribe } = handleCache.add(item, fn);
-    cbArray.push({ cb: cacheId, id });
-    unsubscribeCache.push(unsubscribe);
+
+    // add item and function related to handleCache.
+    const { id: cacheId, unsubscribe } = handleCache.add(item, currentCallback);
+
+    // Update main callback array.
+    // index and frame is settled on first run of tween
+    arrayOfCallback.push({ cb: cacheId, id, index: -1, frame: -1 });
+
+    // Update unsubscribeCache store.
+    unsubscribeCacheArray.push(unsubscribe);
 
     const prevId = id;
+
+    // Update Incremental id.
     callBackStore.set('id', id + 1);
 
     return {
-        unsubscribeCache,
-        unsubscribeCb: (arr) => {
+        unsubscribeCache: unsubscribeCacheArray,
+        unsubscribeCb: (arrayOfCallback) => {
+            // runsubscribe item from handleCache.
             unsubscribe();
+
             // Disable single stagger without modify staggers order
-            return arr.map(({ id, cb, index, frame }) => {
-                if (id === prevId) cb = () => {};
+            return arrayOfCallback.map(({ id, cb, index, frame }) => {
+                if (id === prevId) cb = -1;
                 return { id, cb, index, frame };
             });
         },
