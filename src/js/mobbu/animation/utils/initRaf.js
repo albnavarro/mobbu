fca4e0117c2bdfc26d1fea47d4cef7066054cfca
alberto <alberto.navaro@gmail.com>
2023-08-10 22:02:29 +0200

    Feat(mobbu) jsDoc / ts-check

--- src/js/mobbu/animation/utils/initRaf.js
+++ src/js/mobbu/animation/utils/initRaf.js
@@ -1,14 +1,32 @@
+// @ts-check
+
 import { handleFrame } from '../../events/rafutils/handleFrame.js';
 import { handleNextTick } from '../../events/rafutils/handleNextTick.js';
 
-export const initRaf = (cbPauseArr, rafFn, pauseFn, res) => {
+/**
+ * @param {Array.<{cb:function,id:number,index:Number,frame:Number}>} callbackPauseArray
+ * @param {Function} rafFunction
+ * @param {Function} pauseFunction
+ * @param {Promise.<undefined>} resolve
+ * @returns {void}
+ *
+ * @description
+ * Fire mian requenst animation frame function.
+ * In case the is some function add form timeline fire pauseFUnction.
+ */
+export const initRaf = (
+    callbackPauseArray,
+    rafFunction,
+    pauseFunction,
+    resolve
+) => {
     handleFrame.add(() => {
         handleNextTick.add(({ time, fps }) => {
-            const prevent = cbPauseArr
+            const prevent = callbackPauseArray
                 .map(({ cb }) => cb())
                 .some((item) => item === true);
-            rafFn(time, fps, res);
-            if (prevent) pauseFn();
+            rafFunction(time, fps, resolve);
+            if (prevent) pauseFunction();
         });
     });
 };
