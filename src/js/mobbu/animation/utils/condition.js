f0fda9616bf6e8e5be5eb136838cdb42b68b2ec3
alberto <alberto.navaro@gmail.com>
2023-08-10 20:38:05 +0200

    Feat(animationsUtils) ts-check / jsDoc.

--- src/js/mobbu/animation/utils/condition.js
+++ src/js/mobbu/animation/utils/condition.js
@@ -1,10 +1,27 @@
+// @ts-check
+
 import { checkType } from '../../store/storeType';
 import { staggerEachWarning } from './warning';
 
-export const shouldInizializzeStagger = (each, firstRun, arr1, arr2) => {
+/**
+ * @param {Number} each
+ * @param {Boolean} firstRun
+ * @param {Array.<{cb:function,id:number,index:Number,frame:Number}>} arrayToCompare1
+ * @param {Array.<{cb:function,id:number,index:Number,frame:Number}>} arrayToCompare2
+ */
+export const shouldInizializzeStagger = (
+    each,
+    firstRun,
+    arrayToCompare1,
+    arrayToCompare2
+) => {
     if (!checkType(Number, each)) {
         staggerEachWarning();
     }
 
-    return each > 0 && firstRun && (arr1.length || arr2.length);
+    return (
+        each > 0 &&
+        firstRun &&
+        (arrayToCompare1.length || arrayToCompare2.length)
+    );
 };
