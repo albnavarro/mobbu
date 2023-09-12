43ad65a15488efdfa98adb89e4abec2c7178fe70
alberto <alberto.navaro@gmail.com>
2023-09-12 18:17:58 +0200

    Chore(horizontalScrolelr) Delete old unused state.

--- src/js/component/pages/horizontalScroller/horizontalScrollerButton/definition.js
+++ src/js/component/pages/horizontalScroller/horizontalScrollerButton/definition.js
@@ -4,15 +4,11 @@ import { HorizontalScrollerButton } from './horizontalScrollerButton';
 export const horizontalScrollerButtonDef = createComponent({
     name: 'HorizontalScrollerButton',
     component: HorizontalScrollerButton,
-    exportState: ['id', 'callback', 'active'],
+    exportState: ['id', 'active'],
     state: {
         id: () => ({
             value: -1,
             type: Number,
-        }),
-        callback: () => ({
-            value: () => {},
-            type: Function,
         }),
         active: () => ({
             value: false,
