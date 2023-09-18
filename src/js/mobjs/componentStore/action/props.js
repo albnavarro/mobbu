fd611b62082514657afa5922b93e30cb7ca56687
alberto <alberto.navaro@gmail.com>
2023-09-18 17:33:22 +0200

    Feat(mobJs) componentStore clean step3.

--- src/js/mobjs/componentStore/action/props.js
+++ src/js/mobjs/componentStore/action/props.js
@@ -1,33 +1,8 @@
 // @ts-check
 
-import { storeAction } from '../../createComponent';
-import { componentMap, componentStore } from '../store';
+import { componentMap } from '../store';
 
 export const setDynamicPropsWatch = ({ id = '', unWatchArray = [] }) => {
-    componentStore[storeAction](
-        'instances',
-        (
-            /** @type {Array.<import('../store.js').componentStoreType >} */ prevInstances
-        ) => {
-            return prevInstances.map((item) => {
-                const { id: currentId, parentPropsWatcher } = item;
-
-                // Assign is if existe a parent component and current parentId is null/undefined
-                return id === currentId
-                    ? {
-                          ...item,
-
-                          parentPropsWatcher: [
-                              ...parentPropsWatcher,
-                              ...unWatchArray,
-                          ],
-                      }
-                    : item;
-            });
-        }
-    );
-
-    // - new
     const item = componentMap.get(id);
     const { parentPropsWatcher } = item;
     componentMap.set(id, {
@@ -48,21 +23,6 @@ export const setDynamicPropsWatch = ({ id = '', unWatchArray = [] }) => {
 export const unBind = ({ id = '' }) => {
     if (!id || id === '') return;
 
-    const { instances } = componentStore.get();
-
-    /**
-     * @type {import('../store.js').componentStoreType}
-     */
-    const { parentPropsWatcher } =
-        instances.find(({ id: currentId }) => {
-            return currentId === id;
-        }) || {};
-
-    parentPropsWatcher.forEach((unwatch) => {
-        unwatch();
-    });
-
-    // - new
     const { parentPropsWatcher: parentPropsWatcher2 } = componentMap.get(id);
     parentPropsWatcher2.forEach((unwatch) => {
         unwatch();
