4c855078492182e6e2ef1b571b73e714a3c80f94
alberto <alberto.navaro@gmail.com>
2023-10-01 15:51:31 +0200

    Feat(mobMotion) ts-check fpsLoadedLog utils.

--- src/js/mobMotion/animation/utils/fpsLogInizialization.js
+++ src/js/mobMotion/animation/utils/fpsLogInizialization.js
@@ -1,3 +1,11 @@
+// @ts-check
+
+/**
+ * @param {string} label
+ * @param {number} fps
+ *
+ * @returns {void}
+ */
 export const fpsLoadedLog = (label, fps) => {
     console.log(`stagger on ${label} loaded at: ${fps} fps`);
 };
