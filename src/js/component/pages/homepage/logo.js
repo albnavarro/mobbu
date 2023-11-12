0aa3811e47cdd7d897c067371e8542d78f390fce
alberto <alberto.navaro@gmail.com>
2023-11-12 22:53:31 +0100

    Feat(home) add last svg.

--- src/js/component/pages/homepage/logo.js
+++ src/js/component/pages/homepage/logo.js
@@ -1,8 +1,13 @@
 import logo from '../../../../svg/logov3.svg';
+import pieceArrow from '../../../../svg/piece-arrow.svg';
 
 /**
  * @param {import('../../../mobjs/type').componentType}
  */
 export const HomeLogo = ({ html }) => {
-    return html` <div class="l-index__logo">${logo}</div> `;
+    return html`<div>
+        <div class="l-index__top-left">${pieceArrow}</div>
+        <div class="l-index__logo">${logo}</div>
+        <div class="l-index__top-right">${pieceArrow}</div>
+    </div>`;
 };
