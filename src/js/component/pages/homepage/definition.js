4fa9f087292c3b9423156283c18bbcce425d4919
alberto <alberto.navaro@gmail.com>
2023-11-12 19:12:10 +0100

    Feat(home)

--- src/js/component/pages/homepage/definition.js
+++ src/js/component/pages/homepage/definition.js
@@ -1,13 +1,7 @@
 import { createComponent } from '../../../mobjs';
-import { HomeContent } from './homeContent';
 import { HomeLogo } from './logo';
 
 export const homePageLogoDef = createComponent({
     name: 'home-logo',
     component: HomeLogo,
-});
-
-export const homePageComponentDef = createComponent({
-    name: 'home-content',
-    component: HomeContent,
 });
--- src/js/pages/home/index.js
+++ src/js/pages/home/index.js
@@ -3,6 +3,5 @@ import { html } from '../../mobjs';
 export const home = () => {
     return html`<div class="l-index">
         <home-logo></home-logo>
-        <home-content></home-content>
     </div>`;
 };
