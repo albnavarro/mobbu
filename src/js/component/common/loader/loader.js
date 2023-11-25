6c1ed97f4a4c9b89b8e83e88cb44d66c79fa0a2c
alberto <alberto.navaro@gmail.com>
2023-11-25 15:38:37 +0100

    Feat(loader) add basic tween.

--- src/js/component/common/loader/loader.js
+++ src/js/component/common/loader/loader.js
@@ -1,11 +1,30 @@
+import { tween } from '../../../mobMotion';
+
 /**
  * @param {import("../../../mobjs/type").componentType}
  */
 export const Loader = ({ onMount, html, watch, remove }) => {
-    onMount(() => {
-        watch('shouldRemove', (shouldRemove) => {
+    onMount(({ element }) => {
+        let tweenOut = tween.createTween({
+            data: { opacity: 1, scale: 1 },
+            duration: 500,
+        });
+
+        tweenOut.subscribe(({ opacity, scale }) => {
+            element.style.opacity = opacity;
+            element.style.transform = `scale(${scale})`;
+        });
+
+        watch('shouldRemove', async (shouldRemove) => {
+            await tweenOut.goTo({ opacity: 0, scale: 0.9 });
+
             if (shouldRemove) remove();
         });
+
+        return () => {
+            tweenOut.destroy();
+            tweenOut = null;
+        };
     });
 
     return html`
