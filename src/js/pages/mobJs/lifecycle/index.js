9914aedc5092d45614f64c881d80a3d2e558bad5
alberto <alberto.navaro@gmail.com>
2023-12-02 15:28:33 +0100

    Chore(Docs)

--- src/js/pages/mobJs/lifecycle/index.js
+++ src/js/pages/mobJs/lifecycle/index.js
@@ -1,7 +1,14 @@
 import { html, staticProps } from '../../../mobjs';
 
 export const mobJs_lifecycle = () => {
-    return html`<html-content
-        ${staticProps({ source: '/data/mobJs/lifecycle.json' })}
-    ></html-content>`;
+    return html` <doc-container>
+        <html-content
+            slot="docs"
+            ${staticProps({
+                source: '/data/mobJs/lifecycle.json',
+                useMaxWidth: true,
+            })}
+        ></html-content>
+        <doc-title slot="section-title">Component</doc-title>
+    </doc-container>`;
 };
