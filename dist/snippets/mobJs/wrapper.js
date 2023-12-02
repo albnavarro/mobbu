a24c620e88e7daa8de138b23af0e93c93c6ba39a
alberto <alberto.navaro@gmail.com>
2023-12-02 15:01:52 +0100

    Chore(Docs).

--- dist/snippets/mobJs/wrapper.js
+++ dist/snippets/mobJs/wrapper.js
@@ -1,11 +1,17 @@
-import { html } from '../mobjs';
+/**
+ * Normally these utilities are passed directly as component
+ * parameters, since the wrapper is not a component
+ * we can make use of global utilities.
+ */
+import { staticProps, html } from '../mobjs';
 
 export const wrapper = () => {
     return html`
         <my-header></my-header>
         <main class="main">
+            <!-- Page root -->
             <div id="content"></div>
         </main>
-        <my-footer></my-footer>
+        <my-footer ${staticProps({ myProp: 'content' })}></my-footer>
     `;
 };
