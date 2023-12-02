ba47d071a62923cb7a8cf50bd319b92499d87e03
alberto <alberto.navaro@gmail.com>
2023-12-02 12:25:13 +0100

    Feat(Doc) add base inizializations mobJs.

--- src/js/pages/mobJs/initialization/index.js
+++ src/js/pages/mobJs/initialization/index.js
@@ -1,7 +1,14 @@
 import { html, staticProps } from '../../../mobjs';
 
 export const mobJs_initialization = () => {
-    return html`<html-content
-        ${staticProps({ source: '/data/mobJs/initialization.json' })}
-    ></html-content>`;
+    return html` <doc-container>
+        <html-content
+            slot="docs"
+            ${staticProps({
+                source: '/data/mobJs/initialization.json',
+                useMaxWidth: true,
+            })}
+        ></html-content>
+        <doc-title slot="section-title">Initialization</doc-title>
+    </doc-container>`;
 };
