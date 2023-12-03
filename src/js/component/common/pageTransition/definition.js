59fef5b4d07df72d2e41bf81c47b78a2347b68b1
alberto <alberto.navaro@gmail.com>
2023-12-03 15:55:27 +0100

    Fix(Pagetranstion) reload page.

--- src/js/component/common/pageTransition/definition.js
+++ src/js/component/common/pageTransition/definition.js
@@ -11,6 +11,7 @@ export const pageTransitionComponentDef = createComponent({
         url: () => ({
             value: '',
             type: String,
+            skipEqual: false,
         }),
     },
 });
