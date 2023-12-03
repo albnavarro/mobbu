4980fef4abb2dd9a098d9eee25c4508e4a399f32
alberto <alberto.navaro@gmail.com>
2023-12-03 22:27:18 +0100

    Fix(Docs) section nav.

--- src/js/component/layout/docsContainer/docContainer.js
+++ src/js/component/layout/docsContainer/docContainer.js
@@ -2,8 +2,11 @@
  * @param {import("../../../mobjs/type").componentType}
  */
 export const DocContainer = ({ html, onMount }) => {
-    onMount(() => {
+    onMount(({ refs }) => {
         window.scrollTo(0, 0);
+
+        const { side } = refs;
+        side.classList.add('active');
     });
 
     return html`
@@ -11,7 +14,7 @@ export const DocContainer = ({ html, onMount }) => {
             <div class="c-doc-container__content">
                 <mobjs-slot name="docs"></mobjs-slot>
             </div>
-            <div class="c-doc-container__side">
+            <div class="c-doc-container__side" ref="side">
                 <doc-scroll></doc-scroll>
                 <doc-top></doc-top>
                 <mobjs-slot name="section-title-small"></mobjs-slot>
