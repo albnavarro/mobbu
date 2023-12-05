81ba27bb113e8136c890e0805d745d29aa5c86ab
alberto <alberto.navaro@gmail.com>
2023-12-05 21:43:45 +0100

    Feat(Docs) add watchSync.

---
+++ dist/snippets/mobJs/watchSync.js
@@ -0,0 +1,23 @@
+/**
+ * @param {import("../../../src/js/mobjs/type").componentType}
+ */
+export const MyComponent = ({ html, onMount, watchSync }) => {
+    onMount(({ refs }) => {
+        const { labelRef } = refs;
+
+        /**
+         * React to the state mutation.
+         */
+        watchSync('label', (value, oldValue) => {
+            labelRef.textContent = value;
+        });
+
+        return () => {};
+    });
+
+    return html`
+        <div>
+            <h2 ref="labelRef"></h2>
+        </div>
+    `;
+};
