3617ec6942d79a885c06f89ed2af9945bd1b8097
alberto <alberto.navaro@gmail.com>
2023-12-06 10:56:38 +0100

    Chore(Docs) repeat base.

---
+++ dist/snippets/mobJs/repeat.js
@@ -0,0 +1,51 @@
+import { html } from '../mobjs';
+
+function getItems({ sync, bindProps, delegateEvents }) {
+    return html`
+        <my-child-component
+            ${bindProps({
+                bind: ['counter'],
+                props: ({ counter, _current, _index }) => {
+                    return {
+                        counter,
+                        label: _current.label,
+                        index: _index,
+                    };
+                },
+            })}
+            ${delegateEvents({
+                click: (_e, { current, index }) => console.log(current, index),
+            })}
+            ${sync}
+        >
+        </my-child-component>
+    `;
+}
+
+/**
+ * @param {import('../../../../mobjs/type').componentType}
+ */
+export const MyComponent = ({ html, repeat, bindProps, delegateEvents }) => {
+    return html`
+        <div>
+            ${repeat({
+                watch: 'myArray',
+                clean: false,
+                key: 'myKey',
+                beforeUpdate: ({ container, childrenId }) => {
+                    //
+                },
+                afterUpdate: ({ childrenId, element }) => {
+                    //
+                },
+                render: ({ sync }) => {
+                    return getItems({
+                        sync,
+                        bindProps,
+                        delegateEvents,
+                    });
+                },
+            })}
+        </div>
+    `;
+};
