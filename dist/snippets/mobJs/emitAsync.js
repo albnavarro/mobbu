3e77caefd40a4557b54f7446b2fcc1f01a8a1be9
alberto <alberto.navaro@gmail.com>
2023-12-06 13:57:09 +0100

    Feat(Docs) emitAsync.

---
+++ dist/snippets/mobJs/emitAsync.js
@@ -0,0 +1,13 @@
+/**
+ * @param {import("../mobjs/type").componentType}
+ */
+export const MyComponent = ({ html, onMount, emitAsync }) => {
+    onMount(async () => {
+        await emitAsync('myState');
+        console.log('watcher to myState executed');
+    });
+    /**
+     * DOM component structure.
+     */
+    return html` <div>my component.</div> `;
+};
