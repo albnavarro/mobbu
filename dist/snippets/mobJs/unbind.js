680b88ef929d0ba1381519b56598157afbd65be4
alberto <alberto.navaro@gmail.com>
2023-12-06 13:46:29 +0100

    Feat(Docs) unbind.

---
+++ dist/snippets/mobJs/unbind.js
@@ -0,0 +1,14 @@
+/**
+ * @param {import("../mobjs/type").componentType}
+ */
+export const MyComponent = ({ html, onMount, unBind }) => {
+    onMount(() => {
+        setTimeout(() => {
+            unBind();
+        }, 1000);
+    });
+    /**
+     * DOM component structure.
+     */
+    return html` <div>my component.</div> `;
+};
