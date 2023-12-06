d957a57eabcc10059743141034184179817b835c
alberto <alberto.navaro@gmail.com>
2023-12-06 17:35:00 +0100

    Feat(mobJs) getParentId.

---
+++ dist/snippets/mobJs/getParentId.js
@@ -0,0 +1,15 @@
+/**
+ * @param {import("../mobjs/type").componentType}
+ */
+export const MyComponent = ({ html, onMount, getParentId }) => {
+    onMount(() => {
+        const parentID = getParentId();
+
+        //
+        console.log(parentID);
+    });
+    /**
+     * DOM component structure.
+     */
+    return html` <div>my component.</div> `;
+};
