f15cabac70db1ec30bb90511037d608b5ef68600
alberto <alberto.navaro@gmail.com>
2023-12-07 15:49:06 +0100

    Feat(Docs) debug.

--- dist/snippets/mobJs/debug.js
+++ dist/snippets/mobJs/debug.js
@@ -1,1 +1,13 @@
-test;
+import { setDefaultComponent, componentMap } from '../mobjs';
+
+setDefaultComponent({
+    /**
+     * Add data-mobjs="<id>" to each component
+     */
+    debug: true,
+});
+
+/**
+ * All instance component specs.
+ */
+console.log(componentMap);
