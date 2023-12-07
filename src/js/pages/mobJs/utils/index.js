8ca55ecc501ac4ce6d8ff5ae14d1773fa110db40
alberto <alberto.navaro@gmail.com>
2023-12-07 15:52:52 +0100

    Feat(Docs) utils base.

--- src/js/pages/mobJs/utils/index.js
+++ src/js/pages/mobJs/utils/index.js
@@ -1,7 +1,17 @@
 import { html, staticProps } from '../../../mobjs';
 
 export const mobJs_utils = () => {
-    return html`<html-content
-        ${staticProps({ source: '/data/mobJs/utils.json' })}
-    ></html-content>`;
+    return html` <doc-container>
+        <html-content
+            slot="docs"
+            ${staticProps({
+                source: '/data/mobJs/utils.json',
+                useMaxWidth: true,
+            })}
+        ></html-content>
+        <doc-title-small slot="section-title-small"
+            ><a href="/#mobJs_overview">mobjs</a> / <span>utils</span>
+        <scroll-to slot="section-links"></scroll-to>
+        <doc-title slot="section-title">Utils</doc-title>
+    </doc-container>`;
 };
