5bce697be7b0b379cce8261e5b1fd167b3156578
alberto <alberto.navaro@gmail.com>
2023-12-07 14:24:21 +0100

    Feat(Slot) base

--- src/js/pages/mobJs/slot/index.js
+++ src/js/pages/mobJs/slot/index.js
@@ -1,7 +1,17 @@
 import { html, staticProps } from '../../../mobjs';
 
 export const mobJs_slot = () => {
-    return html`<html-content
-        ${staticProps({ source: '/data/mobJs/slot.json' })}
-    ></html-content>`;
+    return html` <doc-container>
+        <html-content
+            slot="docs"
+            ${staticProps({
+                source: '/data/mobJs/slot.json',
+                useMaxWidth: true,
+            })}
+        ></html-content>
+        <doc-title-small slot="section-title-small"
+            ><a href="/#mobJs_overview">mobjs</a> / <span>slot</span>
+        <scroll-to slot="section-links"></scroll-to>
+        <doc-title slot="section-title">slot</doc-title>
+    </doc-container>`;
 };
