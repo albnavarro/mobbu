89e0b9a4ec7cee1f944a77f85e7ffbe621933703
alberto <alberto.navaro@gmail.com>
2023-12-07 11:58:16 +0100

    Feat(Docs)

--- dist/snippets/mobJs/activeRoute.js
+++ dist/snippets/mobJs/activeRoute.js
@@ -1,9 +1,3 @@
 import { mainStore } from '../js/mobjs';
 
-const unsubscribe = mainStore.watch('activeRoute', (routename) => {
-    //
-});
-
 const { activeRoute } = mainStore.get();
-
-unsubscribe();
