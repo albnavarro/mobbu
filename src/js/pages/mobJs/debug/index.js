fbf3ef5def7bb8744b701ba8a451a62db6fdb7cc
alberto <alberto.navaro@gmail.com>
2023-12-07 15:27:01 +0100

    Feat(Docs) debug.

--- src/js/pages/mobJs/debug/index.js
+++ src/js/pages/mobJs/debug/index.js
@@ -1,7 +1,17 @@
 import { html, staticProps } from '../../../mobjs';
 
 export const mobJs_debug = () => {
-    return html`<html-content
-        ${staticProps({ source: '/data/mobJs/debug.json' })}
-    ></html-content>`;
+    return html` <doc-container>
+        <html-content
+            slot="docs"
+            ${staticProps({
+                source: '/data/mobJs/debug.json',
+                useMaxWidth: true,
+            })}
+        ></html-content>
+        <doc-title-small slot="section-title-small"
+            ><a href="/#mobJs_overview">mobjs</a> / <span>debug</span>
+        <scroll-to slot="section-links"></scroll-to>
+        <doc-title slot="section-title">Debug</doc-title>
+    </doc-container>`;
 };
