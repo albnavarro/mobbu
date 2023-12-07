b62cc2a9414b5fd9d6e72ade941a05c36c3ff07a
alberto <alberto.navaro@gmail.com>
2023-12-07 14:18:50 +0100

    Feat(Docs)

---
+++ dist/snippets/mobJs/refs.js
@@ -0,0 +1,27 @@
+/**
+ * @param {import("../../../src/js/mobjs/type").componentType}
+ */
+export const MyComponent = ({ html, onMount }) => {
+    onMount(({ refs }) => {
+        const { list, list_item } = refs;
+
+        console.log(list); // <ul>
+        console.log(list_item); // [<li>,<li>,<li>,<li>,<li>,<li>,<li>,<li>,<li>,]
+    });
+
+    return html`
+        <div>
+            <ul ref="list">
+                <li ref="list_item">1</li>
+                <li ref="list_item">2</li>
+                <li ref="list_item">3</li>
+                <li ref="list_item">4</li>
+                <li ref="list_item">5</li>
+                <li ref="list_item">6</li>
+                <li ref="list_item">7</li>
+                <li ref="list_item">8</li>
+                <li ref="list_item">9</li>
+            </ul>
+        </div>
+    `;
+};
--- src/js/pages/mobJs/refs/index.js
+++ src/js/pages/mobJs/refs/index.js
@@ -1,7 +1,17 @@
 import { html, staticProps } from '../../../mobjs';
 
 export const mobJs_refs = () => {
-    return html`<html-content
-        ${staticProps({ source: '/data/mobJs/refs.json' })}
-    ></html-content>`;
+    return html` <doc-container>
+        <html-content
+            slot="docs"
+            ${staticProps({
+                source: '/data/mobJs/refs.json',
+                useMaxWidth: true,
+            })}
+        ></html-content>
+        <doc-title-small slot="section-title-small"
+            ><a href="/#mobJs_overview">mobjs</a> / <span>refs</span>
+        <scroll-to slot="section-links"></scroll-to>
+        <doc-title slot="section-title">refs</doc-title>
+    </doc-container>`;
 };
