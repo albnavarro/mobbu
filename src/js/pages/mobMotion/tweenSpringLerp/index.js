6ea86ebb6f1fb3c086cffee00384828b7f4ac77c
alberto <alberto.navaro@gmail.com>
2023-12-08 10:49:21 +0100

    Chore(Docs)

--- src/js/pages/mobMotion/tweenSpringLerp/index.js
+++ src/js/pages/mobMotion/tweenSpringLerp/index.js
@@ -1,7 +1,19 @@
 import { html, staticProps } from '../../../mobjs';
 
 export const mobMotion_tween_spring_lerp = () => {
-    return html`<html-content
-        ${staticProps({ source: '/data/mobMotion/tweenSpringLerp.json' })}
-    ></html-content>`;
+    return html` <doc-container>
+        <html-content
+            slot="docs"
+            ${staticProps({
+                source: '/data/mobMotion/tweenSpringLerp.json',
+                useMaxWidth: true,
+            })}
+        ></html-content>
+        <doc-title-small slot="section-title-small"
+            ><a href="/#mobMotion_overview">mobMotion</a> /
+            <span>Tween Spring Lerp</span></doc-title-small
+        >
+        <scroll-to slot="section-links"></scroll-to>
+        <doc-title slot="section-title">Tweens</doc-title>
+    </doc-container>`;
 };
