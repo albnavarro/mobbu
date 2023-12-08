29b18e9cc7cf746df9195e419136f227436e32d8
alberto <alberto.navaro@gmail.com>
2023-12-08 10:35:50 +0100

    Chore(Docs)

--- src/js/pages/mobMotion/overview/index.js
+++ src/js/pages/mobMotion/overview/index.js
@@ -1,7 +1,17 @@
 import { html, staticProps } from '../../../mobjs';
 
 export const mobMotion_overview = () => {
-    return html`<html-content
-        ${staticProps({ source: '/data/mobMotion/overview.json' })}
-    ></html-content>`;
+    return html` <doc-container>
+        <html-content
+            slot="docs"
+            ${staticProps({
+                source: '/data/mobMotion/overview.json',
+                useMaxWidth: true,
+            })}
+        ></html-content>
+        <doc-title-small slot="section-title-small"
+            >mobMotion 
+        <scroll-to slot="section-links"></scroll-to>
+        <doc-title slot="section-title">mobMotion</doc-title>
+    </doc-container>`;
 };
