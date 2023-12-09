fbda6da786ece08dfc85be4aacf3a38b6ddd9673
alberto <alberto.navaro@gmail.com>
2023-12-09 10:15:16 +0100

    Feat(Docs)

---
+++ dist/snippets/mobMotion/staggerInterface.js
@@ -0,0 +1,18 @@
+interface stagger {
+    type?: 'equal' | 'start' | 'end' | 'center';
+    each?: number;
+    waitComplete?: boolean;
+    from?:
+        | 'start'
+        | 'end'
+        | 'center'
+        | 'edges'
+        | 'random'
+        | { x: number, y: number }
+        | number;
+    grid?: {
+        col: number,
+        row: number,
+        direction: 'row' | 'col' | 'radial',
+    };
+}
