d4105d4861fd36d7036cb3f5c50d669d75234a30
alberto <alberto.navaro@gmail.com>
2023-12-09 12:44:17 +0100

    Feat(Docs)

--- dist/snippets/mobMotion/goTo.js
+++ dist/snippets/mobMotion/goTo.js
@@ -4,6 +4,8 @@ myTween.goTo({ x: 10, rotate: 90 }, { duration: 500 }).then(() => {
 });
 
 // e.g. spring
-mySpring.goTo({ x: 10, rotate: 90 }, { config: 'wobbly' }).then(() => {
-    //
-});
+mySpring
+    .goTo({ x: 10, rotate: (val) => val }, { config: 'wobbly' })
+    .then(() => {
+        //
+    });
