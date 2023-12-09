5061b01fd955fa0ca010c84e2ee0f9a2a0560450
alberto <alberto.navaro@gmail.com>
2023-12-09 14:23:31 +0100

    Feat(Docs)

---
+++ dist/snippets/mobMotion/sequencerGoFromTo.js
@@ -0,0 +1,10 @@
+/**
+ * Create and set the goTo value.
+ */
+const mySequencer = tween
+    .createSequencer({
+        data: { x: 10, y: 0, rotate: 0 },
+        duration: 10,
+        ease: 'easeInQuad',
+    })
+    .goFromTo({ x: 0 }, { x: 100 }, { start: 2, end: 5, ease: 'easeInExpo' });
