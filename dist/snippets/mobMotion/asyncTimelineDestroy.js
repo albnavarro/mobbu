7c2ae6514eddce1d881b5ddeb88aad82ef578702
alberto <alberto.navaro@gmail.com>
2023-12-10 17:23:48 +0100

    Feat(Docs)

---
+++ dist/snippets/mobMotion/asyncTimelineDestroy.js
@@ -0,0 +1,8 @@
+/*
+ * Play.
+ * Resolve after 10 cycle.
+ */
+myTimeline.destroy();
+myTimeline = null;
+myTween = null;
+mySpring = null;
---
+++ dist/snippets/mobMotion/asyncTimelineSetTween.js
@@ -0,0 +1,5 @@
+myTimeline.setTween('myLabel', [myTween, mySpring]).then(() => {
+    myTimeline.playFrom('lamyLabel').then(() => {
+        //
+    });
+});
