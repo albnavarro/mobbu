7e674e5e8af40be4215f450413800ee0a136256b
alberto <alberto.navaro@gmail.com>
2023-12-11 12:09:00 +0100

    Feat(Docs)

--- dist/snippets/mobMotion/lerp.js
+++ dist/snippets/mobMotion/lerp.js
@@ -1,30 +1,9 @@
 import { tween } from '../mobMotion';
 
-const myTween = tween.createLerp({
+const myLerp = tween.createLerp({
     data: { x: 0, y: 0, rotate: 0 },
     velocity: 0.06,
     precision: 0.01,
     relative: false,
     stagger: { each: 7 },
 });
-
-/**
- * Schema:
- *
- * const myLerp = new HandleLerp({
- *   data: Object.<string, number>,
- *   precision?: Number,
- *   velocity?: Number,
- *   relative?: Boolean,
- *   stagger?:{
- *      each?: Number,
- *      from?: Number|String|{x:number,y:number},
- *      grid?: {
- *          col?: Number,
- *          row?: Number,
- *          direction?: String,
- *      },
- *      waitComplete?: Boolean,
- *   },
- * })
- */
---
+++ dist/snippets/mobMotion/lerpType.d.ts
@@ -0,0 +1,24 @@
+interface spring {
+    data: { [key: string]: number | (() => number) };
+    relative?: boolean;
+    precision?: number;
+    velocity?: number;
+    stagger?: {
+        type?: 'equal' | 'start' | 'end' | 'center';
+        each?: number;
+        waitComplete?: boolean;
+        from?:
+            | 'start'
+            | 'end'
+            | 'center'
+            | 'edges'
+            | 'random'
+            | { x: number; y: number }
+            | number;
+        grid?: {
+            col: number;
+            row: number;
+            direction: 'row' | 'col' | 'radial';
+        };
+    };
+}
