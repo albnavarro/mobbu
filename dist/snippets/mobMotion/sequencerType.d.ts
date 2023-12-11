036c2bb90f7ef56bc006be63bce57b09bdd57349
alberto <alberto.navaro@gmail.com>
2023-12-11 12:12:47 +0100

    Feat(Docs).

---
+++ dist/snippets/mobMotion/sequencerType.d.ts
@@ -0,0 +1,54 @@
+interface sequencer {
+    data: { [key: string]: number | (() => number) };
+    duration?: number;
+    ease?:
+        | 'easeLinear'
+        | 'easeInQuad'
+        | 'easeOutQuad'
+        | 'easeInOutQuad'
+        | 'easeInCubic'
+        | 'easeOutCubic'
+        | 'easeInOutCubic'
+        | 'easeInQuart'
+        | 'easeOutQuart'
+        | 'easeInOutQuart'
+        | 'easeInQuint'
+        | 'easeOutQuint'
+        | 'easeInOutQuint'
+        | 'easeInSine'
+        | 'easeOutSine'
+        | 'easeInOutSine'
+        | 'easeInExpo'
+        | 'easeOutExpo'
+        | 'easeInOutExpo'
+        | 'easeInCirc'
+        | 'easeOutCirc'
+        | 'easeInOutCirc'
+        | 'easeInElastic'
+        | 'easeOutElastic'
+        | 'easeInOutElastic'
+        | 'easeInBack'
+        | 'easeOutBack'
+        | 'easeInOutBack'
+        | 'easeInBounce'
+        | 'easeOutBounce'
+        | 'easeInOutBounce';
+    stagger?: {
+        type?: 'equal' | 'start' | 'end' | 'center';
+        each?: number;
+        waitComplete?: boolean;
+        from?:
+            | 'start'
+            | 'end'
+            | 'center'
+            | 'edges'
+            | 'random'
+            | { x: number; y: number }
+            | number;
+        grid?: {
+            col: number;
+            row: number;
+            direction: 'row' | 'col' | 'radial';
+        };
+    };
+}
