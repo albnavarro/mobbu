d1169faf3afb800bcf142c2641ac1d7289c7a60e
alberto <alberto.navaro@gmail.com>
2023-12-11 09:56:08 +0100

    Feat(Docs)

---
+++ dist/snippets/mobMotion/syncTimelinePlayFrom.js
@@ -0,0 +1,9 @@
+/**
+ * Play timeline from label.
+ */
+mytimeline.playFrom('myLabel').then(() => {});
+
+/**
+ * Play reverse timeline from label.
+ */
+mytimeline.playFromReverse('myLabel').then(() => {});
