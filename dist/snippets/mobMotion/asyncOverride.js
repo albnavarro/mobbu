acfd95837eed8d4008216deea57dd795c6791ea0
alberto <alberto.navaro@gmail.com>
2023-12-11 12:28:43 +0100

    Feat(Docs).

--- dist/snippets/mobMotion/asyncOverride.js
+++ dist/snippets/mobMotion/asyncOverride.js
@@ -1,38 +1,14 @@
-/*
- * myTimeline[action](
- *      myTweenInstance,
- *      { Object.<string, (Number|Function)> },
- *      {
- *          --------------
- *          `Tween`
- *          --------------
- *          ease?: String,
- *          duration?: ( Number|Function ),
- *
- *          --------------
- *          `Spring`
- *          --------------
- *          config?: String,
- *          configProp: {
- *             tension?: Number,
- *             mass?: Number,
- *             friction?: Number,
- *             velocity?: Number,
- *             precision?: Number,
- *          },
- *
- *          --------------
- *          `Lerp`
- *          --------------
- *          precision?: Number,
- *          velocity?: Number,
- *
- *          --------------
- *          `common`
- *          --------------
- *          delay?: Number,
- *      }
- *  )
- *  */
-
-myTimeline.goTo(myTween, { x: 10, y: 10 }, { duration: 2000, delay: 500 });
+/**
+ * We update the parameters of each instance temporarily,
+ * the default values will then be restored when the current action is finished.
+ */
+myTimeline
+    .goTo(myTween, { x: 10, y: 10 }, { duration: 2000, delay: 500 })
+    .goTo(mySpring, { x: 10, y: 10 }, { config: 'gentle' })
+    .goTo(
+        mySpring,
+        { x: 100, y: 200 },
+        { config: 'bounce', configProp: { mass: 10, friction: 2 } }
+    )
+    .goTo(myTween, { x: 300, y: 0 }, { duration: 1000, ease: 'easeInExpo' })
+    .goTo(mySpring, { x: 200, y: 50 }, { config: 'gentle', delay: 100 });
