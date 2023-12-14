89152fd357784bc8df3ed996574bf9e34220bf6a
alberto <alberto.navaro@gmail.com>
2023-12-14 20:45:22 +0100

    Feat(Docs)

---
+++ dist/snippets/mobMotion/scrollTriggerEasing.js
@@ -0,0 +1,36 @@
+import { scroller } from '../../../src/js/mobMotion';
+
+const myTarget = document.querySelector('myTarget');
+
+/**
+ * Create instance.
+ */
+const myScrollTrigger = scroller.createScrollTrigger({
+    item: myTarget,
+    propierties: 'rotate',
+    range: '45deg ',
+    start: 'bottom +50vh -halfHeight',
+    end: 'top',
+
+    /**
+     * Enable ease.
+     */
+    ease: true,
+
+    /**
+     * i.e. Spring
+     * easeType: 'spring',
+     * springConfig: 'gentle', // optional
+     */
+
+    /**
+     * i.e. Lerp
+     */
+    easeType: 'lerp',
+    lerpConfig: 0.06, // optional
+});
+
+/**
+ * Run instance.
+ */
+myScrollTrigger.init();
