8aa4c2a1e06a4a0ff071ac3eac26cf9d1620a007
alberto <alberto.navaro@gmail.com>
2023-12-16 16:54:35 +0100

    Feat(Docs).

---
+++ dist/snippets/mobMotion/defaultMedia.js
@@ -0,0 +1,33 @@
+import { motionCore } from '../../../src/js/mobMotion';
+
+motionCore.setDefault({
+    /**
+     * Default mq:
+     *
+     * {
+     *     "xSmall": 320,
+     *     "small": 360,
+     *     "medium": 600,
+     *     "tablet": 768,
+     *     "desktop": 992,
+     *     "large": 1200,
+     *     "xLarge": 1400
+     * }
+     */
+
+    /**
+     * Override and add value.
+     */
+    mq: {
+        desktop: 1024,
+        myBreakpoint: 2200,
+    },
+
+    /**
+     * Override default value for scrollTrigger/paralalx.
+     */
+    defaultMq: {
+        value: 'myBreakpoint',
+        type: 'min',
+    },
+});
