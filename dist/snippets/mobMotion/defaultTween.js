a0402dd92607f518d91e625ef22ab866b36c11da
alberto <alberto.navaro@gmail.com>
2023-12-16 17:09:40 +0100

    Feat(Docs).

---
+++ dist/snippets/mobMotion/defaultTween.js
@@ -0,0 +1,9 @@
+import { motionCore } from '../../../src/js/mobMotion';
+
+motionCore.setDefault({
+    tween: {
+        duration: 1000, // Default is 1000.
+        ease: 'easeLinear', // Default is easeLinear.
+        relative: false, // Default is false.
+    },
+});
