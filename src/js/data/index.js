a48b8ff17661f951a91678e7e5a270b08b1c9c30
alberto <alberto.navaro@gmail.com>
2023-12-17 20:43:08 +0100

    Fix(fetch)

--- src/js/data/index.js
+++ src/js/data/index.js
@@ -7,12 +7,12 @@ export const getLegendData = () => legendData;
  * Load common data.
  */
 export const loadData = async () => {
-    commonData = await fetch(`../data/common.json`)
+    commonData = await fetch(`./data/common.json`)
         .then((response) => response.json())
         .then((data) => data)
         .catch((error) => console.warn('Something went wrong.', error));
 
-    legendData = await fetch(`../data/legend.json`)
+    legendData = await fetch(`./data/legend.json`)
         .then((response) => response.json())
         .then((data) => data)
         .catch((error) => console.warn('Something went wrong.', error));
