a1101a9efd520b571997d9098b071727a07b6db4
alberto <alberto.navaro@gmail.com>
2024-01-07 12:33:32 +0100

    Fix(snipepts) fix routeLink snippet.

--- docs/snippets/mobJs/routingLink.js
+++ docs/snippets/mobJs/routingLink.js
@@ -2,7 +2,5 @@
 
 /**
  * Add params
- * Using parameters with an <a> tag causes the reload of whole page.
- * To avoid this use the loadUrl() function
  */
 <a href="/#my-route?param1=10&param2=1">my route</a>;
