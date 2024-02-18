69ce6f9fedfa744a09e2aa12e0e7c7339ac9157c
alberto <alberto.navaro@gmail.com>
2024-02-18 18:10:58 +0100

    Chore(mobJs) add some nextTick alternatives.

--- src/js/mobCore/utils/nextTick.js
+++ src/js/mobCore/utils/nextTick.js
@@ -7,3 +7,21 @@
 export const useNextLoop = (fn) => {
     setTimeout(() => fn());
 };
+
+// https://macarthur.me/posts/navigating-the-event-loop
+
+/**
+ * alternative 1
+ * Safari not support at moment.
+ * requestIdleCallback(() => {
+ *     fn();
+ * });
+ */
+
+/**
+ * alternative 2:
+ *
+ * const channel = new MessageChannel();
+ * channel.port1.onmessage = fn;
+ * channel.port2.postMessage('');
+ */
