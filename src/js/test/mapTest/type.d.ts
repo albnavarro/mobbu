ee1087efb244a4f75130945f1d71683de25363ce
alberto <alberto.navaro@gmail.com>
2024-04-06 12:21:37 +0200

    feat(mapUtils) update utils.

--- src/js/test/mapTest/type.d.ts
+++ src/js/test/mapTest/type.d.ts
@@ -1,14 +1,20 @@
 export type updateState = <T, K>(arg0: {
     key: T;
     map: Map<T, K>;
-    action: (arg0: { key: T; map: Map<T, K>; state: K }) => K;
+    update: (arg0: { key: T; map: Map<T, K>; state: K }) => K;
 }) => K | undefined;
 
 export type updateStateByProp = <T, K>(arg0: {
     prop: string;
     value: any;
     map: Map<T, K>;
-    action: (arg0: { key: T; map: Map<T, K>; state: K }) => K;
+    exlcludeKey?: T;
+    update: (arg0: { key: T; map: Map<T, K>; state: K }) => K;
+}) => void;
+
+export type updateAll = <T, K>(arg0: {
+    map: Map<T, K>;
+    update: (arg0: { key: T; map: Map<T, K>; state: K }) => K;
 }) => void;
 
 export type myMap = Map<number, { value: string; active: boolean }>;
