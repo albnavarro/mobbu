53b6acdde75afecc2bd013220e96cce5b450eb4f
albnavarro <alberto.navaro@gmail.com>
2024-06-03 22:35:10 +0200

    chore(docs)

---
+++ docs/snippets/mobJs/utils12.js
@@ -0,0 +1,10 @@
+import { useComponent } from '../mobjs';
+
+useComponent([Header, Footer]);
+
+export const wrapper = async () => {
+    return html`
+        <mob-header></mob-header>
+        <mob-footer></mob-footer>
+    `;
+};
