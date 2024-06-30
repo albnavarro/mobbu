66f08b7f0694d1ae3d5b27f86421104ca7ba9380
albnavarro <alberto.navaro@gmail.com>
2024-06-30 19:31:51 +0200

    Fix(bindProps): fix return object type.

--- src/js/mobjs/tsUtils/mobComponentProps.d.ts
+++ src/js/mobjs/tsUtils/mobComponentProps.d.ts
@@ -1,6 +1,6 @@
 import { bindEventsObject } from '../temporaryData/bindEvents/type';
 import { delegateEventObject } from '../temporaryData/weakBindEvents/type';
-import { NotValue, OnlyStringKey, RempapToOptional } from './utils';
+import { NotValue, OnlyStringKey } from './utils';
 
 /**
  * bindProps.
@@ -8,7 +8,7 @@ import { NotValue, OnlyStringKey, RempapToOptional } from './utils';
 export type PartialBindProps<T, R> = (arg0: {
     bind: Array<OnlyStringKey<T>>;
     forceParent?: boolean;
-    props: (arg0: T & { _current: any; _index: number }) => RempapToOptional<R>;
+    props: (arg0: T & { _current: any; _index: number }) => R | object;
 }) => string;
 
 /**
