b66f2214ef88db080b0a3639923dbdc886ff859a
albnavarro <alberto.navaro@gmail.com>
2024-08-14 21:05:22 +0200

    feat(MapUtils): update type utils

--- src/js/test/mapTest/type.d.ts
+++ src/js/test/mapTest/type.d.ts
@@ -1,23 +1,23 @@
-export type updateState = <T, K>(arg0: {
-    key: T;
-    map: Map<T, K>;
-    update: (arg0: { key: T; map: Map<T, K>; state: K }) => K;
-    set: (arg0: { key: T; state: K }) => void;
-}) => K | undefined;
+export type updateState = <K, O>(arg0: {
+    key: K;
+    map: Map<K, O>;
+    update: (arg0: { key: K; map: Map<K, O>; state: O }) => O;
+    set: (arg0: { key: K; state: O }) => void;
+}) => O | undefined;
 
-export type updateStateByProp = <T, K>(arg0: {
-    prop: string;
-    value: any;
-    map: Map<T, K>;
-    exclude?: T[];
-    update: (arg0: { key: T; map: Map<T, K>; state: K }) => K;
-    set: (arg0: { key: T; state: K }) => void;
+export type updateStateByProp = <K, O, P extends keyof O>(arg0: {
+    prop: P;
+    value: O[P];
+    map: Map<K, O>;
+    exclude?: K[];
+    update: (arg0: { key: K; map: Map<K, O>; state: O }) => O;
+    set: (arg0: { key: K; state: O }) => void;
 }) => void;
 
-export type updateAll = <T, K>(arg0: {
-    map: Map<T, K>;
-    update: (arg0: { key: T; map: Map<T, K>; state: K }) => K;
-    set: (arg0: { key: T; state: K }) => void;
+export type updateAll = <K, O>(arg0: {
+    map: Map<K, O>;
+    update: (arg0: { key: K; map: Map<K, O>; state: O }) => O;
+    set: (arg0: { key: K; state: O }) => void;
 }) => void;
 
 export type myMap = Map<number, { value: string; active: boolean }>;
