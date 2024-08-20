2ddf80d5454c6798dd813519eed3647ec8a693f9
albnavarro <alberto.navaro@gmail.com>
2024-08-20 12:58:07 +0200

    fix(mobJs): snippet

--- docs/snippets/mobJs/utils-use-method-by-name.js
+++ docs/snippets/mobJs/utils-use-method-by-name.js
@@ -6,6 +6,10 @@ import { useMethodByName } from './mobjs';
  */
 export const MyComponent = ({ onMount, html }) => {
     onMount(() => {
+        /**
+         * It is a good idea to use the Optional chaining to ensure that the
+         * component is mounted if you use it directly inside the onMount() function.
+         */
         useMethodByName('otherComponent')?.myMethod();
         return () => {};
     });
