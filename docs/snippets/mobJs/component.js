4ed4e5fcb3527494fcc61ad80fa0f3e99a533cb1
albnavarro <alberto.navaro@gmail.com>
2024-10-14 11:38:11 +0200

    chore(DOCS)

--- docs/snippets/mobJs/component.js
+++ docs/snippets/mobJs/component.js
@@ -4,46 +4,23 @@
 export const MyComponentFn = ({
     html,
     onMount,
-    setState,
-    getState,
+    updateState,
     staticProps,
     bindProps,
     watch,
     delegateEvents,
     setRef,
     getRef,
+    bindText,
 }) => {
-    /**
-     * Label state initial value.
-     */
-    const { label } = getState();
-
-    /**
-     * getState(): Get component current state.
-     * setState(): Update component state.
-     * staticProps(): Set a non reactive child props.
-     * bindProps(): Set reactive child props.
-     * watch(): React to state change.
-     * delegateEvents(): Use event like click etc...
-     */
-
-    /**
-     * Function fired at the end of all component parse.
-     * Here all components is attached to the DOM (if scoped params is disabled).
-     * element: root DOM element.
-     * refs: Object with all refs.
-     */
     onMount(({ element }) => {
         const { labelRef } = getRef();
 
         console.log(element); // div.
         console.log(labelRef); // h2.
 
-        /**
-         * Update h2.
-         */
-        watch('label', (value) => {
-            labelRef.textContent = value;
+        watch('myState', (value) => {
+            labelRef.classList.toggle('my-class', value);
         });
 
         /**
@@ -59,11 +36,11 @@ export const MyComponentFn = ({
         <div>
             <h2
                 ${delegateEvents({
-                    click: () => setState('label', 'new label value'),
+                    click: () => updateState('myState', (value) => !value),
                 })}
                 ${setRef('labelRef')}
             >
-                ${label}
+                ${bindText`label: ${'label'}`}
             </h2>
             <child-component
                 ${staticProps({
