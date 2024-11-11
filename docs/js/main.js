7dda65f01735ab9f06f9778e54fed5f7f77f5bc8
albnavarro <alberto.navaro@gmail.com>
2024-11-11 21:30:53 +0100

    feat(move3D): add basic style and symbols

--- docs/js/main.js
+++ docs/js/main.js
@@ -31512,10 +31512,51 @@ Loading snippet ...</pre
   };
 
   // src/js/component/pages/move3D/Move3D.js.js
-  var Move3Dfn = ({ onMount, html }) => {
+  var Move3Dfn = ({ onMount, html, getState, setRef, getRef }) => {
+    const {
+      yLimit,
+      xLimit,
+      yDepth,
+      xDepth,
+      perspective,
+      centerToViewoport,
+      drag
+    } = getState();
+    const pageY = drag && centerToViewoport;
+    let height = 0;
+    let width = 0;
+    let offSetLeft = 0;
+    let offSetTop = 0;
+    let delta = 0;
+    let limit = 0;
+    let lastX = 0;
+    let dragX = 0;
+    let lastY = 0;
+    let dragY = 0;
+    let onDrag = false;
+    let firstDrag = false;
+    const spring = tween.createSpring({ data: { ax: 0, ay: 0 } });
     onMount(() => {
+      const { container } = getRef();
+      const unsubscribeSpring = spring.subscribe(({ ax, ay }) => {
+        container.style.transform = `translate3D(0,0,0) rotateY(${ax}deg) rotateX(${ay}deg)`;
+      });
+      const unsubscribeOnComplete = spring.onComplete(({ ax, ay }) => {
+        container.style.transform = `rotateY(${ax}deg) rotateX(${ay}deg)`;
+      });
+      return () => {
+        unsubscribeSpring();
+        unsubscribeOnComplete();
+        spring.destroy();
+      };
     });
-    return html`<div class="c-move-3d">move3d test</div>`;
+    return html`<div class="c-move-3d">
+        <div class="c-move-3d__scene">
+            <div class="c-move-3d__container" ${setRef("container")}>
+                container
+            </div>
+        </div>
+    </div>`;
   };
 
   // src/js/component/pages/move3D/definition.js
--- src/js/component/pages/move3D/Move3D.js.js
+++ src/js/component/pages/move3D/Move3D.js.js
@@ -1,15 +1,65 @@
 //@ts-check
 
+import { tween } from '../../../mobMotion';
+
 /**
  * @import { MobComponent} from '../../../mobjs/type';
  **/
 
 /** @type {MobComponent<import('./type').Move3D>} */
-export const Move3Dfn = ({ onMount, html }) => {
-    onMount(() => {});
+export const Move3Dfn = ({ onMount, html, getState, setRef, getRef }) => {
+    const {
+        yLimit,
+        xLimit,
+        yDepth,
+        xDepth,
+        perspective,
+        centerToViewoport,
+        drag,
+    } = getState();
+
+    const pageY = drag && centerToViewoport;
+
+    let height = 0;
+    let width = 0;
+    let offSetLeft = 0;
+    let offSetTop = 0;
+    let delta = 0;
+    let limit = 0;
+    let lastX = 0;
+    let dragX = 0;
+    let lastY = 0;
+    let dragY = 0;
+    let onDrag = false;
+    let firstDrag = false;
+    const spring = tween.createSpring({ data: { ax: 0, ay: 0 } });
+
+    onMount(() => {
+        const { container } = getRef();
+
+        const unsubscribeSpring = spring.subscribe(({ ax, ay }) => {
+            container.style.transform = `translate3D(0,0,0) rotateY(${ax}deg) rotateX(${ay}deg)`;
+        });
+
+        const unsubscribeOnComplete = spring.onComplete(({ ax, ay }) => {
+            container.style.transform = `rotateY(${ax}deg) rotateX(${ay}deg)`;
+        });
+
+        return () => {
+            unsubscribeSpring();
+            unsubscribeOnComplete();
+            spring.destroy();
+        };
+    });
 
     /**
      * Desktop
      */
-    return html`<div class="c-move-3d">move3d test</div>`;
+    return html`<div class="c-move-3d">
+        <div class="c-move-3d__scene">
+            <div class="c-move-3d__container" ${setRef('container')}>
+                container
+            </div>
+        </div>
+    </div>`;
 };
