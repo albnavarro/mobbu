0c0f23f1a2c895bfb25758f413e92e9417460394
albnavarro <alberto.navaro@gmail.com>
2025-11-09 12:45:47 +0100

    fix(DOCS): fix mobStore DOCS.

--- docs/snippets/mob-core/get-proxi.js
+++ docs/snippets/mob-core/get-proxi.js
@@ -9,6 +9,12 @@ const myStore = MobCore.createStore({
         value: { prop: 2 },
         type: 'any',
     }),
+    myComplexObj: {
+        prop: () => ({
+            value: 2,
+            type: Number,
+        }),
+    },
 });
 
 const proxi = myStore.getProxi();
@@ -25,11 +31,10 @@ proxi.myProp = 'test value 2';
 proxi.myProp = 'test value 3';
 
 /**
- * For nested object is recommended use set/update
- * Since the proxy does not implement deep watch, it is necessary
- * to reassign the object
+ * - Since the proxy does not implement deep watch, it is necessary to reassign the object
  */
-proxi.myObj = { prop: 3 };
-proxi.myObj = { ...proxi.myObj, prop: 4 };
+proxi.myObj = { prop: 3 }; // wrong
+proxi.myObj = { ...proxi.myObj, prop: 4 }; // right
+proxi.myComplexObj = { ...proxi.myComplexObj, prop: 4 }; // right
 
 console.log(proxi.myObj.prop);
