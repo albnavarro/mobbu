82d473bdc44b656836ebbea05895ed97b40d4e9b
albnavarro <alberto.navaro@gmail.com>
2025-11-16 14:57:58 +0100

    feat(style): add stylelint-config-property-sort-order-smacss

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,9 @@
 /** @type {import('stylelint').Config} */
 export default {
-    extends: 'stylelint-config-standard-scss',
+    extends: [
+        'stylelint-config-standard-scss',
+        'stylelint-config-property-sort-order-smacss',
+    ],
     rules: {
         // 'at-rule-no-unknown': null,
     },
