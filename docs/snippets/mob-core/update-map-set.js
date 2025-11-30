3fa49835113dcdbbcffee8e531dbc915bdb1c0ab
albnavarro <alberto.navaro@gmail.com>
2025-11-30 16:44:29 +0100

    chore(DOCS): update

--- docs/snippets/mob-core/update-map-set.js
+++ docs/snippets/mob-core/update-map-set.js
@@ -4,9 +4,20 @@ const myStore = MobCore.createStore({
     myMap: () => ({
         value: new Map(),
         type: Map,
-        skipEqual: false,
+        skipEqual: false, // !important
     }),
 });
 
 const proxi = myStore.getProxi();
+
+myStore.watch(
+    () => proxi.myMap,
+    (current, previous) => {
+        // current and previous is equal, map is not reassigned.
+        console.log(current === previous);
+    }
+);
+
 myStore.update(proxi.myMap, (value) => value.set('myKey', 10));
+myStore.set(proxi.myMap, proxi.myMap.set('myKey', 11));
+proxi.myMap = proxi.myMap.set('myKey', 12);
