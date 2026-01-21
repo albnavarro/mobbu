bb58a3df5948e1ae40a020f7dafa4c6c785eae00
albnavarro <alberto.navaro@gmail.com>
2026-01-21 23:38:34 +0100

    feat(mobJs): load component, add comment & better transform set to object statement.

--- src/js/mob/mob-js/component/component-list.js
+++ src/js/mob/mob-js/component/component-list.js
@@ -6,6 +6,11 @@ import { defineBindTextComponent } from '../web-component/bind-text';
 import { defineBindObjectComponent } from '../web-component/bind-object';
 
 /**
+ * Available component:
+ *
+ * - Key is the tag of component.
+ * - Value is the specs.
+ *
  * @type {{ [key: string]: import('../main-store/type').ComponentListMap }}
  */
 let componentListMap = {};
@@ -23,20 +28,19 @@ export const getComponentList = () => {
 const availableComponent = new Set();
 
 /**
- * Inizalize components
+ * Inizalize components from availableComponent data Set.
  *
  * @returns {void}
  */
 export const setComponentList = () => {
-    componentListMap = [...availableComponent.values()].reduce(
-        (previous, current) => ({ ...previous, ...current }),
-        {}
+    componentListMap = Object.fromEntries(
+        [...availableComponent.values()].flatMap((item) => Object.entries(item))
     );
 
     console.log(`component loaded:${Object.keys(componentListMap).length}`);
 
     /**
-     * Register custom HTML tag component. Thios custom TAG will be converted in native DOM element during parse.
+     * Register custom HTML tag component. This custom TAG will be converted in native DOM element during parse.
      */
 
     defineUserComponent(componentListMap);
@@ -48,6 +52,10 @@ export const setComponentList = () => {
 };
 
 /**
+ * - Single page call useComponent first, so load first level component used in page.
+ * - Single component load from page use useComponent for child component inside createComponent() function.
+ * - At the end all component is loaded correctly.
+ *
  * @param {import('../type').CreateComponentReturnType[]} components
  * @returns {void}
  */
