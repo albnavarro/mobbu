9973768a53c27aff39da57407a8925c5c2204d72
albnavarro <alberto.navaro@gmail.com>
2026-02-16 20:20:50 +0100

    feat(mobJs): repeat/invalidate, remove initialize function reference once is consumed.

--- src/js/mob/mob-js/modules/invalidate/action/set-invalidate-instances-map-initialized.js
+++ src/js/mob/mob-js/modules/invalidate/action/set-invalidate-instances-map-initialized.js
@@ -14,8 +14,15 @@ export const setInvalidateInstancesMapInitialized = ({ invalidateId }) => {
     const item = invalidateInstancesMap.get(invalidateId);
     if (!item) return;
 
+    /**
+     * After module is initialized:
+     *
+     * - Set flag to true.
+     * - Remove reference to initialize function.
+     */
     invalidateInstancesMap.set(invalidateId, {
         ...item,
         initialized: true,
+        initializeModule: () => {},
     });
 };
--- src/js/mob/mob-js/modules/repeater/action/set-repeater-instances-map-initialized.js
+++ src/js/mob/mob-js/modules/repeater/action/set-repeater-instances-map-initialized.js
@@ -13,8 +13,15 @@ export const setRepeaterInstancesMapInitialized = ({ repeatId }) => {
     const item = repeatInstancesMap.get(repeatId);
     if (!item) return;
 
+    /**
+     * After module is initialized:
+     *
+     * - Set flag to true.
+     * - Remove reference to initialize function.
+     */
     repeatInstancesMap.set(repeatId, {
         ...item,
         initialized: true,
+        initializeModule: () => {},
     });
 };
