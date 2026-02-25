c57a2f8931505569cec7a999ecacdce0f2c80060
albnavarro <alberto.navaro@gmail.com>
2026-02-25 20:37:43 +0100

    cleanup(store): remove unnecessary copy strategy.

--- src/js/mob/mob-core/store/store-map.js
+++ src/js/mob/mob-core/store/store-map.js
@@ -1,10 +1,4 @@
-import { deepClone } from './clone-obj';
-import {
-    STORE_STRATEGY_CUSTOM_COPY,
-    STORE_STRATEGY_DEEP_COPY,
-    STORE_STRATEGY_SHALLOW_COPY,
-    storeCopyStrategy,
-} from './strategy';
+import { STORE_STRATEGY_SHALLOW_COPY, storeCopyStrategy } from './strategy';
 
 /**
  * @type {import('./type').StoreMap}
@@ -25,44 +19,6 @@ export const getStateFromMainMap = (id) => {
          * Sallow-copy of wrapper object
          */
         return valueNow ? { ...valueNow } : undefined;
-    }
-
-    if (storeCopyStrategy === STORE_STRATEGY_CUSTOM_COPY) {
-        const valueNow = storeMap.get(id);
-
-        /**
-         * Sallow-copy of wrapper store and validationStatusObject
-         */
-        return valueNow
-            ? {
-                  ...valueNow,
-                  store: { ...valueNow.store },
-                  validationStatusObject: {
-                      ...valueNow.validationStatusObject,
-                  },
-              }
-            : undefined;
-    }
-
-    if (storeCopyStrategy === STORE_STRATEGY_DEEP_COPY) {
-        const valueNow = storeMap.get(id);
-
-        /**
-         * Deep-copy of wrapper store and validationStatusObject
-         *
-         * - Store && validationStatusObject is the only object where propierties should be overridden.
-         * - Test only overridden object not new insert or delete propierties
-         * - Primiteves is copy with shallow-copy by defat.
-         */
-        return valueNow
-            ? {
-                  ...valueNow,
-                  store: deepClone(valueNow.store),
-                  validationStatusObject: deepClone(
-                      valueNow.validationStatusObject
-                  ),
-              }
-            : undefined;
     }
 
     /**
--- src/js/mob/mob-core/store/strategy.js
+++ src/js/mob/mob-core/store/strategy.js
@@ -9,37 +9,12 @@ export const STORE_STRATEGY_DEFAULT = 'store_default';
  * SHALLOW COPY
  *
  * Create a copy of `wrapper` witch contains data.
- *
- * - Propierties refer to original data. Proxies use original state in map
  */
 export const STORE_STRATEGY_SHALLOW_COPY = 'store_shallow_copy';
 
 /**
- * CUSTOM COPY
  *
- * In getStateFromMainMap() is possible return a custom copy of data.
- */
-export const STORE_STRATEGY_CUSTOM_COPY = 'store_custom_copy';
-
-/**
- * DEEP COPY
- *
- * Deep copy of store and validationStatusObject for testing purpose.
- */
-export const STORE_STRATEGY_DEEP_COPY = 'store_deep_copy';
-
-/**
- * BOTH CASE:
- *
- * In both case, for consistency, data is update like use CUSTOM COPY strategy.
  *
  * @type {string}
  */
 export const storeCopyStrategy = STORE_STRATEGY_SHALLOW_COPY;
-
-/**
- * Proxies need fresh copy of store each time set && get is invoked.
- */
-export const storeStrategyNeedCopy = () =>
-    storeCopyStrategy === STORE_STRATEGY_CUSTOM_COPY ||
-    storeCopyStrategy === STORE_STRATEGY_DEEP_COPY;
