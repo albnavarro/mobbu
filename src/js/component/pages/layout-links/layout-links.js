d6446ee4fc7304dc78028dc150312662feebfc3d
albnavarro <alberto.navaro@gmail.com>
2026-04-11 17:33:25 +0200

    feat(app) links layout switch to fromObject

--- src/js/component/pages/layout-links/layout-links.js
+++ src/js/component/pages/layout-links/layout-links.js
@@ -1,4 +1,4 @@
-import { html } from '@mobJs';
+import { fromObject } from '@mobJs';
 import { linksScroller } from './animation/links-scroller';
 import { MobCore } from '@mobCore';
 
@@ -71,87 +71,116 @@ export const LayoutLinksFn = ({
         };
     });
 
-    return html`<div class="l-links">
-        <div class="top">
-            <div
-                class="top-title"
-                ${bindEffect({
-                    toggleClass: {
-                        'is-visible': () => proxi.isMounted,
-                    },
-                })}
-            >
-                ${proxi.title}
-            </div>
-        </div>
-        <div class="grid">
-            <div class="grid-item" ${setRef('screenElement')}>
-                <div
-                    class="grid-bottom"
-                    ${bindEffect({
-                        toggleClass: {
-                            active: () => proxi.isMounted,
-                        },
-                    })}
-                ></div>
-                <div
-                    class="title"
-                    ${bindEffect({
+    return fromObject({
+        className: 'l-links',
+        content: [
+            {
+                className: 'top',
+                content: {
+                    className: 'top-title',
+                    modules: bindEffect({
                         toggleClass: {
                             'is-visible': () => proxi.isMounted,
                         },
-                    })}
-                >
-                    <h1 class="title-big">${proxi.title}</h1>
-                </div>
-                <div
-                    class="scrollable-element"
-                    ${setRef('scrollerElement')}
-                    ${bindEffect({
-                        toggleClass: {
-                            'use-drag-cursor': () => proxi.showControls,
-                        },
-                    })}
-                >
-                    <ul class="items">
-                        ${proxi.items
-                            .map((item, index) => {
-                                return html`
-                                    <li class="item">
-                                        <a
-                                            class="${getItemClass(index)}"
-                                            href="${item.url}"
-                                            ${bindEffect({
-                                                toggleClass: {
-                                                    active: () =>
-                                                        proxi.isMounted,
+                    }),
+                    content: proxi.title,
+                },
+            },
+            {
+                className: 'grid',
+                content: [
+                    {
+                        className: 'grid-item',
+                        modules: setRef('screenElement'),
+                        content: [
+                            {
+                                className: 'grid-bottom',
+                                modules: bindEffect({
+                                    toggleClass: {
+                                        active: () => proxi.isMounted,
+                                    },
+                                }),
+                            },
+                            {
+                                className: 'title',
+                                modules: bindEffect({
+                                    toggleClass: {
+                                        'is-visible': () => proxi.isMounted,
+                                    },
+                                }),
+                                content: {
+                                    tag: 'h1',
+                                    className: 'title-big',
+                                    content: proxi.title,
+                                },
+                            },
+                            {
+                                className: 'scrollable-element',
+                                modules: [
+                                    setRef('scrollerElement'),
+                                    bindEffect({
+                                        toggleClass: {
+                                            'use-drag-cursor': () =>
+                                                proxi.showControls,
+                                        },
+                                    }),
+                                ],
+                                content: {
+                                    tag: 'ul',
+                                    className: 'items',
+                                    content: proxi.items
+                                        .map((item, index) => {
+                                            return fromObject({
+                                                tag: 'li',
+                                                className: 'item',
+                                                content: {
+                                                    tag: 'a',
+                                                    className:
+                                                        getItemClass(index),
+                                                    attributes: {
+                                                        href: item.url,
+                                                    },
+                                                    modules: bindEffect({
+                                                        toggleClass: {
+                                                            active: () =>
+                                                                proxi.isMounted,
+                                                        },
+                                                    }),
+                                                    content: [
+                                                        {
+                                                            tag: 'span',
+                                                            className: `counter index-${index}`,
+                                                            content:
+                                                                getCounter(
+                                                                    index
+                                                                ),
+                                                        },
+                                                        {
+                                                            tag: 'span',
+                                                            className: `index-${index}`,
+                                                            content: item.title,
+                                                        },
+                                                    ],
                                                 },
-                                            })}
-                                        >
-                                            <span class="counter index-${index}"
-                                                >${getCounter(index)}</span
-                                            >
-                                            <span class="index-${index}"
-                                                >${item.title}</span
-                                            >
-                                        </a>
-                                    </li>
-                                `;
-                            })
-                            .join('')}
-                    </ul>
-                </div>
-            </div>
-            <h6
-                class="scroll-label"
-                ${bindEffect({
-                    toggleClass: {
-                        active: () => proxi.showControls,
+                                            });
+                                        })
+                                        .join(''),
+                                },
+                            },
+                        ],
                     },
-                })}
-            >
-                Scroll or drag
-            </h6>
-        </div>
-    </div>`;
+                    {
+                        tag: 'h6',
+                        className: 'scroll-label',
+                        modules: bindEffect({
+                            toggleClass: {
+                                active: () => proxi.showControls,
+                            },
+                        }),
+                        content: 'Scroll or drag',
+                    },
+                ],
+            },
+        ],
+    });
 };
