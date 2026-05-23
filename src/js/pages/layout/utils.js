439ff0773f9263ea7f3e112f291275d3bc3997d4
albnavarro <alberto.navaro@gmail.com>
2026-05-23 12:11:47 +0200

    feat(app): add global focus style.

--- src/js/pages/layout/utils.js
+++ src/js/pages/layout/utils.js
@@ -3,19 +3,27 @@
  * @param {import('./type').LayoutSidebarAnchorBreadCrumbs[]} params.breadCrumbs
  */
 export const getBreadCrumbs = ({ breadCrumbs }) => {
+    let backArrow = '';
+
     const items = breadCrumbs
-        .map((item, index) =>
-            index === breadCrumbs.length - 1
-                ? /* HTML */ `<a href="${item.url}" class="arrows">
-                          <div class="arrow-start"></div>
-                          <div class="arrow-end"></div>
-                      </a>
-                      <a class="link" href="${item.url}">${item.title}</a>`
-                : /* HTML */ `<a class="link" href="${item.url}"
-                      >${item.title}</a
-                  >`
-        )
+        .map((item, index) => {
+            /**
+             * Extract back arrow.
+             *
+             * - Back arrow should tay always in first DOM position
+             */
+            if (index === breadCrumbs.length - 1) {
+                backArrow = /* HTML */ `<a href="${item.url}" class="arrows">
+                    <div class="arrow-start"></div>
+                    <div class="arrow-end"></div>
+                </a>`;
+            }
+
+            return /* HTML */ `<a class="link" href="${item.url}"
+                >${item.title}</a
+            >`;
+        })
         .join('');
 
-    return `<div class="c-breadcrumbs">${items}</div>`;
+    return `<div class="c-breadcrumbs">${backArrow}${items}</div>`;
 };
