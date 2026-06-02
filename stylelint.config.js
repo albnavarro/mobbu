8d89548181f094c0be7252ced43ea291c066824a
albnavarro <alberto.navaro@gmail.com>
2026-06-02 18:46:30 +0200

    feat(style): add stylint-use-logical plugin, fix all code, update all packeges.

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,10 +1,12 @@
 /** @type {import('stylelint').Config} */
 export default {
+    plugins: ['stylelint-use-logical'],
     extends: [
         'stylelint-config-standard-scss',
         'stylelint-config-property-sort-order-smacss',
     ],
     rules: {
         'color-function-alias-notation': null,
+        'csstools/use-logical': 'always',
     },
 };
