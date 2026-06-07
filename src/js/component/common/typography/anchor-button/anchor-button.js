c90dfc3abadf11b83a1ec567a0a538bb72599c05
albnavarro <alberto.navaro@gmail.com>
2026-06-07 23:21:47 +0200

    fix(app): fix anchor button

--- src/js/component/common/typography/anchor-button/anchor-button.js
+++ src/js/component/common/typography/anchor-button/anchor-button.js
@@ -12,42 +12,44 @@ export const AnchorButtonFn = ({ getState, delegateEvents }) => {
     const { content: label, anchor } = getState();
 
     return htmlObject({
-        tag: 'button',
-        className: 'anchor-button',
-        attributes: { type: 'button', role: 'link' },
-        modules: delegateEvents({
-            click: async () => {
-                const target = document.querySelector(anchor);
-                if (!target) return;
+        content: {
+            tag: 'button',
+            className: 'anchor-button',
+            attributes: { type: 'button', role: 'link' },
+            modules: delegateEvents({
+                click: async () => {
+                    const target = document.querySelector(anchor);
+                    if (!target) return;
 
-                // @ts-ignore
-                const offsetTop = offset(target).top - 100;
-                await MobBodyScroll.to(offsetTop, { duration: 10 });
+                    // @ts-ignore
+                    const offsetTop = offset(target).top - 100;
+                    await MobBodyScroll.to(offsetTop, { duration: 10 });
 
-                /**
-                 * Set focus to anchor element
-                 */
-                /** @type {HTMLElement} */ (target).focus({
-                    preventScroll: true,
-                });
-            },
-        }),
-        content: [
-            label,
-            {
-                tag: 'span',
-                className: 'arrows',
-                content: [
-                    {
-                        tag: 'span',
-                        className: 'arrow-start',
-                    },
-                    {
-                        tag: 'span',
-                        className: 'arrow-end',
-                    },
-                ],
-            },
-        ],
+                    /**
+                     * Set focus to anchor element
+                     */
+                    /** @type {HTMLElement} */ (target).focus({
+                        preventScroll: true,
+                    });
+                },
+            }),
+            content: [
+                label,
+                {
+                    tag: 'span',
+                    className: 'arrows',
+                    content: [
+                        {
+                            tag: 'span',
+                            className: 'arrow-start',
+                        },
+                        {
+                            tag: 'span',
+                            className: 'arrow-end',
+                        },
+                    ],
+                },
+            ],
+        },
     });
 };
