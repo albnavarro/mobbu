76dcc6e65f3240cb474084c1061750786517831e
albnavarro <alberto.navaro@gmail.com>
2026-06-14 22:54:42 +0200

    feat(app): hide hscroller test from mobile

--- src/js/pages/plugin/horizontal-scroller/index.js
+++ src/js/pages/plugin/horizontal-scroller/index.js
@@ -1,9 +1,30 @@
+import { OnlyTablet } from '@commonComponent/only-tablet/definition';
 import { updateQuickNavState } from '@commonComponent/quick-nav/utils';
 import { htmlObject, MobJs } from '@mobJs';
+import { MobMotionCore } from '@mobMotion';
 import { HorizontalScroller } from '@pagesComponent/horizontal-scroller/definition';
+import { loadTextContent } from '@utils/utils';
 
 /** @type {import('@mobJsType').PageAsync} */
 export const horizontalScroller = async () => {
+    const { data: bg } = await loadTextContent({
+        source: './asset/svg/lettering-mob.svg?v=1.3',
+    });
+
+    if (MobMotionCore.mq('max', 'medium')) {
+        return htmlObject({
+            content: [
+                {
+                    className: 'l-background-shape',
+                    content: bg,
+                },
+                {
+                    component: OnlyTablet,
+                },
+            ],
+        });
+    }
+
     /** Quicknav */
     updateQuickNavState({
         active: true,
