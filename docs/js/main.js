c4c26b2415b727d92715417342b3b0fc1c056e92
albnavarro <alberto.navaro@gmail.com>
2026-06-14 17:13:38 +0200

    feat(mobile): update

--- docs/js/main.js
+++ docs/js/main.js
@@ -26821,10 +26821,69 @@
   };
 
   // src/js/component/pages/about-mobile/about.js
-  var AboutMobileComponentFn = () => {
+  var AboutMobileComponentFn = ({ getSelfProxi }) => {
+    const proxi = getSelfProxi();
+    console.log(proxi.block_1.titleTop);
+    const sectionOne = {
+      tag: "section",
+      content: [
+        {
+          tag: "h1",
+          content: proxi.block_1.titleTop
+        },
+        {
+          tag: "h2",
+          content: proxi.block_1.titleBottom
+        }
+      ]
+    };
+    const sectionTwo = {
+      tag: "section",
+      content: [
+        {
+          tag: "h2",
+          content: proxi.block_2.title
+        },
+        {
+          tag: "p",
+          content: proxi.block_2.copy
+        }
+      ]
+    };
+    const sectionThree = {
+      tag: "section",
+      content: [
+        {
+          tag: "h2",
+          content: proxi.block_3.title
+        },
+        {
+          tag: "p",
+          content: proxi.block_3.copy
+        }
+      ]
+    };
+    const sectionFour = {
+      tag: "section",
+      content: [
+        {
+          tag: "h2",
+          content: proxi.block_4.title
+        },
+        {
+          tag: "ul",
+          content: proxi.block_4.items.map((item) => {
+            return htmlObject({
+              tag: "li",
+              content: item
+            });
+          })
+        }
+      ]
+    };
     return htmlObject({
       className: "l-about-mobile",
-      content: "pluto"
+      content: [sectionOne, sectionTwo, sectionThree, sectionFour]
     });
   };
 
--- src/js/component/pages/about-mobile/about.js
+++ src/js/component/pages/about-mobile/about.js
@@ -4,10 +4,74 @@
 
 import { htmlObject } from '@mobJs';
 
-/** @type {MobComponent} */
-export const AboutMobileComponentFn = () => {
+/** @type {MobComponent<import('./type').About>} */
+export const AboutMobileComponentFn = ({ getSelfProxi }) => {
+    const proxi = getSelfProxi();
+    console.log(proxi.block_1.titleTop);
+
+    const sectionOne = {
+        tag: 'section',
+        content: [
+            {
+                tag: 'h1',
+                content: proxi.block_1.titleTop,
+            },
+            {
+                tag: 'h2',
+                content: proxi.block_1.titleBottom,
+            },
+        ],
+    };
+
+    const sectionTwo = {
+        tag: 'section',
+        content: [
+            {
+                tag: 'h2',
+                content: proxi.block_2.title,
+            },
+            {
+                tag: 'p',
+                content: proxi.block_2.copy,
+            },
+        ],
+    };
+
+    const sectionThree = {
+        tag: 'section',
+        content: [
+            {
+                tag: 'h2',
+                content: proxi.block_3.title,
+            },
+            {
+                tag: 'p',
+                content: proxi.block_3.copy,
+            },
+        ],
+    };
+
+    const sectionFour = {
+        tag: 'section',
+        content: [
+            {
+                tag: 'h2',
+                content: proxi.block_4.title,
+            },
+            {
+                tag: 'ul',
+                content: proxi.block_4.items.map((item) => {
+                    return htmlObject({
+                        tag: 'li',
+                        content: item,
+                    });
+                }),
+            },
+        ],
+    };
+
     return htmlObject({
         className: 'l-about-mobile',
-        content: 'pluto',
+        content: [sectionOne, sectionTwo, sectionThree, sectionFour],
     });
 };
